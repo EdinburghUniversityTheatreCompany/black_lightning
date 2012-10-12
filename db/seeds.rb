3fa5a095e02c0b76126b5f9a6e4d0d36b9b3ad95
Craig Snowden <craig@craigsnowden.com>
2012-10-12 14:44:34 +0100

    seed the database with some shows so that I don't need to keep on readding them in the console

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,4 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+Show.create([{name: "Accidental Death of an Anarchist", slug: "accidental-death-of-an-anarchist", tagline: "A play by Dario Fo"},{name: "City of Cake", slug: "city-of-cake", tagline: "Cake! CAKE! THIS SHOW HAS CAKE!", xts_id: 419},{name: "Spring Awakening", slug: 'spring-awakening', tagline: "Performances as Teviot Debating Hall", xts_id: 423}])
