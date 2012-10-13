9d19c2d02e4f193449f8fcdffcdc3df8d57acfd9
Craig Snowden <craig@craigsnowden.com>
2012-10-13 15:37:03 +0100

    add is public property to shows

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121013133755) do
+ActiveRecord::Schema.define(:version => 20121013143608) do
 
   create_table "roles", :force => true do |t|
     t.string   "name"
@@ -32,6 +32,7 @@ ActiveRecord::Schema.define(:version => 20121013133755) do
     t.integer  "xts_id"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.boolean  "is_public"
   end
 
   create_table "users", :force => true do |t|
