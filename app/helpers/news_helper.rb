c9532a1518fdac15ffed02a7f276ef52fbeb35f1
Hayden Ball <ball.hayden@gmail.com>
2012-10-13 19:41:28 +0100

    Stop regex being too greedy

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -4,7 +4,7 @@ module NewsHelper
     def generate_preview (content)
         #FIXME: This pattern will only work if content ends in a newline.
         content = content + "\n"
-        /.{,140}.+\n/m.match(content)[0]
+        /.{,140}.+?\n/m.match(content)[0]
     end
 
 end
