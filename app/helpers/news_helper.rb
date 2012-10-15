ca72730ee104ca4d66fc29df656843b4316becba
Hayden Ball <ball.hayden@gmail.com>
2012-10-15 13:54:01 +0100

    Fixed an issue with generate_preview where an empty body would break everything

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -2,9 +2,13 @@ module NewsHelper
 
     # find the first line break after 140 characters
     def generate_preview (content)
-        #FIXME: This pattern will only work if content ends in a newline.
-        content = content + "\n"
-        /.{,140}.+?\n/m.match(content)[0]
+        begin
+            #FIXME: This pattern will only work if content ends in a newline.
+            content = content + "\n"
+            /.{,140}.+?\n/m.match(content)[0]
+        rescue Exception
+            "There was an error rendering a preview for this news item."
+        end
     end
 
 end
