239e8dbd756a0afa19ca988d22ee7042b1f46fab
Hayden Ball <ball.hayden@gmail.com>
2012-10-17 12:06:16 +0100

    Changed views over to render markdown

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -4,8 +4,7 @@ module NewsHelper
     def generate_preview (content)
         begin
             #FIXME: This pattern will only work if content ends in a newline.
-            content = content + "\n"
-            /.{,140}.+?\n/m.match(content)[0]
+            /.{,140}.+?<\/p>/m.match(content)[0]
         rescue Exception
             "There was an error rendering a preview for this news item."
         end
