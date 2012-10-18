1f0a8226d87304eedf97c8a99101b92ae30464e5
Hayden Ball <ball.hayden@gmail.com>
2012-10-18 21:43:44 +0100

    Added editable_block test to homepage

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -4,6 +4,8 @@ class StaticControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
     assert_response :success
+    
+    assert_select "h1", "Welcome to Project BlackLightning"
   end
 
   test "should get about" do
