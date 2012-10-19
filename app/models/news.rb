c4990580be5acf7f32e25a66af48335d9cf0ab99
Hayden Ball <ball.hayden@gmail.com>
2012-10-19 19:54:52 +0100

    Added images to news

--- app/models/news.rb
+++ app/models/news.rb
@@ -8,5 +8,6 @@ class News < ActiveRecord::Base
   validates :publish_date, :presence => true
   validates :slug, :presence => true, :uniqueness => true
 
-  attr_accessible :publish_date, :show_public, :slug, :title, :body
+  has_attached_file :image, :styles => { :medium => "x300>", :thumb => "x100>" }
+  attr_accessible :publish_date, :show_public, :slug, :title, :body, :image
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121019170105) do
+ActiveRecord::Schema.define(:version => 20121019184814) do
 
   create_table "admin_editable_blocks", :force => true do |t|
     t.string   "name"
@@ -26,8 +26,12 @@ ActiveRecord::Schema.define(:version => 20121019170105) do
     t.string   "slug"
     t.datetime "publish_date"
     t.boolean  "show_public"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",         :null => false
+    t.datetime "updated_at",         :null => false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "roles", :force => true do |t|
