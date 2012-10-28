58f909c4f5c7409387d96a51c3814e30c4a8af95
Hayden Ball <ball.hayden@gmail.com>
2012-10-28 18:03:25 +0000

    Added admin.css and login.css to the asset precompile array

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,7 @@ module ChaosRails
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    config.assets.precompile += ['admin.css', 'login.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
