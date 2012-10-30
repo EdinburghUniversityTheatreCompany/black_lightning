a422a951487f3099a709cd0fd9a4f3be4fc5fb67
Craig Snowden <craig@craigsnowden.com>
2012-10-30 00:40:30 +0000

    show slideshow

--- app/models/show.rb
+++ app/models/show.rb
@@ -6,6 +6,6 @@ class Show < ActiveRecord::Base
   
   validates :slug, :presence => true, :uniqueness => true
 
-  has_attached_file :image, :styles => { :medium => "x300>", :thumb => "x100>" }
+  has_attached_file :image, :styles => { :medium => "x300>", :thumb => "x100>", :slideshow => "960x500#" }
   attr_accessible :description, :name, :slug, :tagline, :xts_id, :is_public, :image, :start_date, :end_date
 end
