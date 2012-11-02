4cc43535e2743d2e7653a0912ba653ecf3d2fe9e
Hayden Ball <ball.hayden@gmail.com>
2012-11-02 15:49:11 +0000

    More validation. ref #17

--- app/models/admin/staffing.rb
+++ app/models/admin/staffing.rb
@@ -3,5 +3,7 @@ class Admin::Staffing < ActiveRecord::Base
    
    accepts_nested_attributes_for :staffing_jobs, :reject_if => :all_blank, :allow_destroy => true
    
+   validates :show_title, :date, :presence => true
+   
    attr_accessible :show_title, :date, :staffing_jobs, :staffing_jobs_attributes
 end
