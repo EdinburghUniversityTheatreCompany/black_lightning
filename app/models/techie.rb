b6074c36732bf3620097cfd6782a177f144d39a2
Craig Snowden <craig@craigsnowden.com>
2012-11-05 00:34:29 +0000

    add parental relationship

--- app/models/techie.rb
+++ app/models/techie.rb
@@ -1,4 +1,5 @@
 class Techie < ActiveRecord::Base
   attr_accessible :name
-  has_and_belongs_to_many :children, :class_name => "Techie", :association_foreign_key => "child_id", :join_table => "children_techies"
+  has_and_belongs_to_many :children, :class_name => "Techie", :foreign_key => "techie_id", :association_foreign_key => "child_id", :join_table => "children_techies"
+  has_and_belongs_to_many :parents, :class_name => "Techie", :foreign_key => "child_id", :association_foreign_key => "techie_id", :join_table => "children_techies"
 end
