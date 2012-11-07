300123857076ef02a758e66b3ce28e5c77a782c7
Hayden Ball <ball.hayden@gmail.com>
2012-11-07 22:13:57 +0000

    Changed about page to an editable_block (I may have done this somewhere else as well) and added edit button for editable_blocks

--- app/helpers/editable_block_helper.rb
+++ app/helpers/editable_block_helper.rb
@@ -3,13 +3,17 @@ module EditableBlockHelper
     def display_block (name, admin_page)
         @editable_block = Admin::EditableBlock.find_by_name(name)
         
+        if not @editable_block then
+          return "Block not defined"
+        end
+        
         @editable_block.admin_page = admin_page
         @editable_block.save
-        
-        if @editable_block then
-          return render_markdown(@editable_block.content)
+          
+        if can? :edit, @editable_block then
+          return render :partial => "/shared/editable_block_editor"
         else
-          return "Block not defined"
+          return render_markdown(@editable_block.content)
         end
     end
 
