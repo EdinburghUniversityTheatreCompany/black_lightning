507e19749ebf6988e047fb5e21db517d330c8052
Hayden Ball <ball.hayden@gmail.com>
2012-11-07 16:31:20 +0000

    That looks a bit better

--- app/controllers/admin/job_control_controller.rb
+++ app/controllers/admin/job_control_controller.rb
@@ -3,6 +3,26 @@ class Admin::JobControlController < AdminController
   def overview
   end
 
+  def working
+  end
+
+  def pending
+  end
+
+  def failed
+  end
+
+  def remove
+    delayed_job.find(params[:id]).delete
+    redirect_to :back
+  end
+
+  def retry
+    job = delayed_job.find(params[:id])
+    job.update_attributes(:run_at => Time.now, :failed_at => nil)
+    redirect_to :back
+  end
+
   def delayed_job
     begin
       Delayed::Job
