ee93df38925216070554b33f9d20b92596059ba0
Hayden Ball <ball.hayden@gmail.com>
2012-11-09 18:05:49 +0000

    More styling on Dashboard

--- app/helpers/admin/job_control_helper.rb
+++ app/helpers/admin/job_control_helper.rb
@@ -1,2 +1,26 @@
 module Admin::JobControlHelper
+  def delayed_job
+    begin
+      Delayed::Job
+    rescue
+      false
+    end
+  end
+
+  def delayed_jobs(type)
+    delayed_job.where(delayed_job_sql(type))
+  end
+
+  def delayed_job_sql(type)
+    case type
+    when :enqueued
+      ''
+    when :working
+      'locked_at is not null'
+    when :failed
+      'last_error is not null'
+    when :pending
+      'attempts = 0'
+    end
+  end
 end
