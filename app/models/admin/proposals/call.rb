5a9c9d402d2946550b4bb088f63919fdcd522deb
Hayden Ball <ball.hayden@gmail.com>
2012-11-10 21:22:26 +0000

    Added open named_scope to call

--- app/models/admin/proposals/call.rb
+++ app/models/admin/proposals/call.rb
@@ -1,4 +1,6 @@
 class Admin::Proposals::Call < ActiveRecord::Base
+  name_scope :open, :donditions => { :open => true }
+
   has_and_belongs_to_many :questions, :class_name => "Admin::Proposals::Question"
   
   has_many :proposals, :class_name => "Admin::Proposals::Proposal"
