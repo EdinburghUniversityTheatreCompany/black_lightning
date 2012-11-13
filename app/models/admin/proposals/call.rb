ee42dd65872aa9c3d739d58b7afaa15e1d83bc78
Hayden Ball <ball.hayden@gmail.com>
2012-11-13 14:48:05 +0000

    Closes #23. Won't actually delete the question at the moment, as questions are on a many_to_many relationship (so could theoretically be used in another proposal) but this removes the answers to the question for the current call.

--- app/models/admin/proposals/call.rb
+++ app/models/admin/proposals/call.rb
@@ -1,5 +1,5 @@
 class Admin::Proposals::Call < ActiveRecord::Base
-  has_and_belongs_to_many :questions, :class_name => "Admin::Proposals::Question"
+  has_and_belongs_to_many :questions, :class_name => "Admin::Proposals::Question", :before_remove => :answers_cleanup
 
   has_many :proposals, :class_name => "Admin::Proposals::Proposal"
 
@@ -8,4 +8,11 @@ class Admin::Proposals::Call < ActiveRecord::Base
   validates :deadline, :name, :presence => true
 
   attr_accessible :deadline, :name, :open, :questions, :questions_attributes
+  
+  #Removes answers to questions that have been removed
+  def answers_cleanup(question)
+    self.proposals.each do |proposal|
+      proposal.answers.where(:question_id => question.id).destroy_all
+    end
+  end
 end
