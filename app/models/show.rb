4f2ad2aa20d7ddc6ac811fb588ac6595d3a24e71
Hayden Ball <ball.hayden@gmail.com>
2012-11-23 18:22:21 +0000

    Corrected error caused by date_range in show model if no dates have been specified

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,13 +11,17 @@ class Show < ActiveRecord::Base
 
   accepts_nested_attributes_for :team_members
 
-  validates :name, :description, :presence => true
+  validates :name, :description, :tagline, :start_date, :end_date, :presence => true
   validates :slug, :presence => true, :uniqueness => true
 
   has_attached_file :image, :styles => { :medium => "x300>", :thumb => "x100>", :slideshow => "960x500#" }
   attr_accessible :description, :name, :slug, :tagline, :xts_id, :is_public, :image, :start_date, :end_date, :team_members, :team_members_attributes
 
   def date_range
+    if not self.start_date.presence then
+      return
+    end
+  
     date = I18n.l(self.start_date, :format => :short)
 
     if self.end_date then
