07546537bdc1ccb92686bdba688bfd50cafbd6b2
Hayden Ball <ball.hayden@gmail.com>
2012-11-23 13:45:48 +0000

    Damn you white space

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,8 +1,8 @@
 class ArchivesController < ApplicationController
   layout "archives"
-  
+
   before_filter :check_select_date, :except => [:set_date]
-  
+
   def set_date
     if params[:target]
       redirect_to send('archives_' + params[:target] + '_index_path', params[:start_month], params[:start_year], params[:end_month], params[:end_year])
@@ -10,10 +10,10 @@ class ArchivesController < ApplicationController
       redirect_to archives_index_path(params[:start_month], params[:start_year], params[:end_month], params[:end_year])
     end
   end
-  
+
   def index
   end
-  
+
   private
   def check_select_date
     unless request.env['PATH_INFO'] == archives_index_path
@@ -21,12 +21,12 @@ class ArchivesController < ApplicationController
         redirect_to archives_index_path, notice: 'Please select a date range first'
         return
       end
-      
+
       start_yr = Integer(params[:start_year])
       start_mnth = Integer(params[:start_month])
       end_yr = Integer(params[:end_year])
       end_mnth = Integer(params[:end_month])
-      
+
       @search_start_date = ::Date.new(start_yr, start_mnth, 1)
       @search_end_date = ::Date.new(end_yr, end_mnth, Time.days_in_month(end_mnth))
     end
--- app/models/show.rb
+++ app/models/show.rb
@@ -5,7 +5,7 @@ class Show < ActiveRecord::Base
   end
 
   scope :current, where(["end_date >= ? AND is_public = ?", Date.current, true]).order("start_date ASC")
-  
+
   has_many :team_members, :class_name => "::TeamMember", :as => :teamwork
   has_many :users, :through => :team_members
 
@@ -16,10 +16,10 @@ class Show < ActiveRecord::Base
 
   has_attached_file :image, :styles => { :medium => "x300>", :thumb => "x100>", :slideshow => "960x500#" }
   attr_accessible :description, :name, :slug, :tagline, :xts_id, :is_public, :image, :start_date, :end_date, :team_members, :team_members_attributes
-  
+
   def date_range
-    date = I18n.l(self.start_date, :format => :short) 
-    
+    date = I18n.l(self.start_date, :format => :short)
+
     if self.end_date then
         date << " - "
         date << I18n.l(self.end_date, :format => :short)
