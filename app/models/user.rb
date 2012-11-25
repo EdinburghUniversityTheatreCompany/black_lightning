6d65188e0e254c629b98c18c36831c170fb04748
Hayden Ball <ball.hayden@gmail.com>
2012-11-25 18:05:37 +0000

    Validate phone numbers. Closes #20

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  before_save :unify_numbers
   rolify
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -8,7 +9,7 @@ class User < ActiveRecord::Base
   #set our own validations
   validates :password, :presence => true, :if => lambda { new_record? || !password.nil? || !password.blank? }
   validates :email, :presence => true
-
+  validates :phone_number, :allow_blank => true, :format => { :with => /(\+44\s?7\d{3}|07\d{3})\s?(\d{3}\s?\d{3})$/, :message => "Please enter a valid mobile number" }
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me, :first_name, :last_name, :role_ids, :phone_number
@@ -16,5 +17,13 @@ class User < ActiveRecord::Base
 
   def name
   	"#{self.first_name} #{self.last_name}"
+  end
+
+  def unify_numbers
+    self.phone_number = phone_number.gsub(/\s/, '')
+
+    if self.phone_number[0] == '0' then
+      self.phone_number[0] = '+44'
+    end
   end
 end
