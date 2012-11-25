3c76d276946545e65b192cd0e4a091990fad14ed
Hayden Ball <ball.hayden@gmail.com>
2012-11-25 21:04:43 +0000

    Fixed tests

--- test/functional/admin/proposals/call_question_templates_controller_test.rb
+++ test/functional/admin/proposals/call_question_templates_controller_test.rb
@@ -2,13 +2,17 @@ require 'test_helper'
 
 class Admin::Proposals::CallQuestionTemplatesControllerTest < ActionController::TestCase
   setup do
-    @admin_proposals_call_question_template = admin_proposals_call_question_templates(:one)
+    @template = admin_proposals_call_question_templates(:one)
+
+    @user = User.find_by_email('admin@bedlamtheatre.co.uk')
+    @user.add_role :admin
+    sign_in @user
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:admin_proposals_call_question_templates)
+    assert_not_nil assigns(:templates)
   end
 
   test "should get new" do
@@ -17,31 +21,34 @@ class Admin::Proposals::CallQuestionTemplatesControllerTest < ActionController::
   end
 
   test "should create admin_proposals_call_question_template" do
+    #Remove the existing entry:
+    Admin::Proposals::CallQuestionTemplate.find(@template).destroy
+
     assert_difference('Admin::Proposals::CallQuestionTemplate.count') do
-      post :create, admin_proposals_call_question_template: {  }
+      post :create, template: { name: @template.name }
     end
 
-    assert_redirected_to admin_proposals_call_question_template_path(assigns(:admin_proposals_call_question_template))
+    assert_redirected_to admin_proposals_call_question_template_path(assigns(:template))
   end
 
   test "should show admin_proposals_call_question_template" do
-    get :show, id: @admin_proposals_call_question_template
+    get :show, id: @template
     assert_response :success
   end
 
   test "should get edit" do
-    get :edit, id: @admin_proposals_call_question_template
+    get :edit, id: @template
     assert_response :success
   end
 
   test "should update admin_proposals_call_question_template" do
-    put :update, id: @admin_proposals_call_question_template, admin_proposals_call_question_template: {  }
-    assert_redirected_to admin_proposals_call_question_template_path(assigns(:admin_proposals_call_question_template))
+    put :update, id: @template, template: { name: @template.name }
+    assert_redirected_to admin_proposals_call_question_template_path(assigns(:template))
   end
 
   test "should destroy admin_proposals_call_question_template" do
     assert_difference('Admin::Proposals::CallQuestionTemplate.count', -1) do
-      delete :destroy, id: @admin_proposals_call_question_template
+      delete :destroy, id: @template
     end
 
     assert_redirected_to admin_proposals_call_question_templates_path
