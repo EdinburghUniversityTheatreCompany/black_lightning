5c426f7566df98722078d14e8193e9e3af712642
Hayden Ball <ball.hayden@gmail.com>
2012-11-26 16:35:54 +0000

    Fixed tests for techie families

--- test/functional/admin/techie_families_controller_test.rb
+++ test/functional/admin/techie_families_controller_test.rb
@@ -1,6 +1,12 @@
 require 'test_helper'
 
 class Admin::TechieFamiliesControllerTest < ActionController::TestCase
+  setup do
+    @user = User.find_by_email('admin@bedlamtheatre.co.uk')
+    @user.add_role :admin
+    sign_in @user
+  end
+
   test "should get index" do
     get :index
     assert_response :success
