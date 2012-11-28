2ef14261e3537ee5c6bdb9d36ba727b93522e8c4
Hayden Ball <ball.hayden@gmail.com>
2012-11-28 11:05:00 +0000

    More stuff to precompile

--- config/application.rb
+++ config/application.rb
@@ -55,7 +55,7 @@ module ChaosRails
 
     # Enable the asset pipeline
     config.assets.enabled = true
-    config.assets.precompile += ['admin.css', 'admin.js', 'login.css', 'jquery.lightbox-0.5.js', 'jquery.lightbox-0.5.css', 'admin/dashboard.css', ]
+    config.assets.precompile += ['admin.css', 'admin.js', 'login.css', 'jquery.lightbox-0.5.js', 'jquery.lightbox-0.5.css', 'admin/dashboard.css', 'admin/proposals/call_templates.js', 'admin/questionnaires/questionnaire_templates.js']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
