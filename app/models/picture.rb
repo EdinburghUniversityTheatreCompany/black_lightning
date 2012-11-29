27ea9c8a986f6b0edcf6e9d06ff4ce25190a2f75
Hayden Ball <ball.hayden@gmail.com>
2012-11-29 10:02:52 +0000

    Constrain thumbnail height as well

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :gallery, :polymorphic => true
 
-  has_attached_file :image, :styles => { :thumb => "150>", :display => "700>" }
+  has_attached_file :image, :styles => { :thumb => "150x100", :display => "700>" }
 
   attr_accessible :description, :image
 end
