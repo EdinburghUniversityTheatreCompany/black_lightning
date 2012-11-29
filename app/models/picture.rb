ca435aa7390c327f0a619622ea24df99f5507dc8
Hayden Ball <ball.hayden@gmail.com>
2012-11-29 10:07:23 +0000

    Constrain gallery pictures to be displayed at max of 700x700 (rather than just max width of 700)

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,7 +1,7 @@
 class Picture < ActiveRecord::Base
   belongs_to :gallery, :polymorphic => true
 
-  has_attached_file :image, :styles => { :thumb => "150x100", :display => "700>" }
+  has_attached_file :image, :styles => { :thumb => "150x100", :display => "700x700" }
 
   attr_accessible :description, :image
 end
