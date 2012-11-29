6c5a9aef7c846e93b939b68c1a99bca3e3f4556b
Craig Snowden <craig@craigsnowden.com>
2012-11-29 14:49:57 +0000

    add text messaging

--- app/mailers/staffing_mailer.rb
+++ app/mailers/staffing_mailer.rb
@@ -3,10 +3,18 @@ class StaffingMailer < ActionMailer::Base
 
   def staffing_reminder(staffing)
     @staffing = staffing
+# set up a client to talk to the Twilio REST API
+    client = Twilio::REST::Client.new ChaosRails::Application.config.twilio_account_sid, ChaosRails::Application.config.twilio_auth_token
 
     staffing.staffing_jobs.each do |job|
       @user = job.user
       mail(:to => @user.email, :subject => "Bedlam Theatre Staffing")
+
+      client.account.sms.messages.create(
+	:from => ChaosRails::Application.config.twilio_phone_number,
+	:to => @user.phone_number,
+	:body => "Hey! You're staffing #{job.name} at #{job.staffing.show_title}! GET YO ASS THERE"
+	)
     end
   end
 end
