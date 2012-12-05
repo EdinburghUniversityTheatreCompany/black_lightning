7c3a0919760582fc2e07e44b5caf724141cbe489
Hayden Ball <ball.hayden@gmail.com>
2012-12-05 21:26:52 +0000

    Remove a staffing's reminder_job if the staffing is deleted. Closes #62

--- app/models/admin/staffing.rb
+++ app/models/admin/staffing.rb
@@ -11,23 +11,32 @@
 #
 
 class Admin::Staffing < ActiveRecord::Base
-   default_scope order("date ASC")
+  before_destroy :job_cleanup
 
-   scope :future, where(['date > ?', DateTime.now])
-   scope :past, where(['date < ?', DateTime.now])
+  default_scope order("date ASC")
 
-   has_many :staffing_jobs, :class_name => "Admin::StaffingJob"
+  scope :future, where(['date > ?', DateTime.now])
+  scope :past, where(['date < ?', DateTime.now])
+
+  has_many :staffing_jobs, :class_name => "Admin::StaffingJob"
 
    # Having this as a belongs_to feels wrong, but since the id of the job needs to be stored in the staffing it is necessary.
-   belongs_to  :reminder_job, :class_name => "::Delayed::Job"
+  belongs_to :reminder_job, :class_name => "::Delayed::Job"
 
-   accepts_nested_attributes_for :staffing_jobs, :reject_if => :all_blank, :allow_destroy => true
+  accepts_nested_attributes_for :staffing_jobs, :reject_if => :all_blank, :allow_destroy => true
 
-   validates :show_title, :date, :presence => true
+  validates :show_title, :date, :presence => true
 
-   attr_accessible :show_title, :date, :staffing_jobs, :staffing_jobs_attributes
+  attr_accessible :show_title, :date, :staffing_jobs, :staffing_jobs_attributes
 
-   def filled_jobs
-     self.staffing_jobs.where(['user_id is not null']).count
-   end
+  def filled_jobs
+    self.staffing_jobs.where(['user_id is not null']).count
+  end
+
+  private
+  def job_cleanup
+    if reminder_job then
+      reminder_job.delete
+    end
+  end
 end
