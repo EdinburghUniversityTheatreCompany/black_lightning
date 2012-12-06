43e304920098158fbcef2271b223858070e36b31
Hayden Ball <ball.hayden@gmail.com>
2012-12-06 21:09:50 +0000

    static controller tests

--- test/functional/static_controller_test.rb
+++ test/functional/static_controller_test.rb
@@ -5,4 +5,19 @@ class StaticControllerTest < ActionController::TestCase
     get :home
     assert_response :success
   end
+
+  test "should get 403" do
+    get :access_denied
+    assert_response 403
+  end
+
+  test "should get 404" do
+    get :render_404
+    assert_response 404
+  end
+
+  test "should get 500" do
+    get :render_500
+    assert_response 500
+  end
 end
