3ea4e352c1008f53591627092159a5f4c884a712
Hayden Ball <ball.hayden@gmail.com>
2012-12-06 21:32:01 +0000

    Corrected proposals convert_to_show test now that delayed job is disabled for tests

--- test/unit/admin/proposals/proposal_test.rb
+++ test/unit/admin/proposals/proposal_test.rb
@@ -19,8 +19,7 @@ require 'test_helper'
 class Admin::Proposals::ProposalTest < ActiveSupport::TestCase
   test "convert to show" do
     @admin_proposals_proposal = ::Admin::Proposals::Proposal.find(1)
-    job = @admin_proposals_proposal.convert_to_show()
-    job.invoke_job
+    @admin_proposals_proposal.convert_to_show()
 
     assert ::Show.find_by_name('Test Proposal')
   end
