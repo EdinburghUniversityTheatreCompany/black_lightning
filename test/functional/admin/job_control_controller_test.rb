51888c79ed041b4b6fa7dfcc993ec1c1eeee3d7a
Hayden Ball <ball.hayden@gmail.com>
2012-12-07 22:22:01 +0000

    Added job_control and show archives test

--- test/functional/admin/job_control_controller_test.rb
+++ test/functional/admin/job_control_controller_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
 
 class Admin::JobControlControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  setup do
+    @user = users(:one)
+    @user = User.find_by_email('admin@bedlamtheatre.co.uk')
+    @user.add_role :admin
+    sign_in @user
+  end
+
+  test "should get overview" do
+    get :overview
+    assert_response :success
+  end
+
+  test "should get working" do
+    get :working
+    assert_response :success
+  end
+
+  test "should get pending" do
+    get :pending
+    assert_response :success
+  end
+
+  test "should get failed" do
+    get :failed
+    assert_response :success
+  end
 end
--- test/functional/archives/shows_controller_test.rb
+++ test/functional/archives/shows_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class Archives::ShowsControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should get index" do
+    get :index
+    assert_redirected_to archives_shows_index_path(01, 1.years.ago.year, 12, Date.today.year)
+  end
 end
