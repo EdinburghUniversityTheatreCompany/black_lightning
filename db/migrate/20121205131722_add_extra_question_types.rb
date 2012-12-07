b16cea3dea7020a3b5704d037c810f53ac5ada2e
Hayden Ball <ball.hayden@gmail.com>
2012-12-07 22:06:51 +0000

    Best practices

--- db/migrate/20121205131722_add_extra_question_types.rb
+++ db/migrate/20121205131722_add_extra_question_types.rb
@@ -2,16 +2,20 @@ class AddExtraQuestionTypes < ActiveRecord::Migration
   def self.up
     add_attachment :admin_answers, :file
 
-    ::Admin::Question.update_all("admin_questions.response_type = 'Short Text'", "admin_questions.response_type = 'String'")
-    ::Admin::Question.update_all("admin_questions.response_type = 'Long Text'", "admin_questions.response_type = 'Text'")
-    ::Admin::Question.update_all("admin_questions.response_type = 'Number'", "admin_questions.response_type = 'Integer'")
+    say_with_time("Updating question response_types to new styles") do
+      ::Admin::Question.update_all("admin_questions.response_type = 'Short Text'", "admin_questions.response_type = 'String'")
+      ::Admin::Question.update_all("admin_questions.response_type = 'Long Text'", "admin_questions.response_type = 'Text'")
+      ::Admin::Question.update_all("admin_questions.response_type = 'Number'", "admin_questions.response_type = 'Integer'")
+    end
   end
 
   def self.down
     remove_attachment :admin_answers, :file
 
-    ::Admin::Question.update_all("admin_questions.response_type = 'String'", "admin_questions.response_type = 'Short Text'")
-    ::Admin::Question.update_all("admin_questions.response_type = 'Text'", "admin_questions.response_type = 'Long Text'")
-    ::Admin::Question.update_all("admin_questions.response_type = 'Integer'", "admin_questions.response_type = 'Number'")
+    say_with_time("Reverting question response_types to old styles") do
+      ::Admin::Question.update_all("admin_questions.response_type = 'String'", "admin_questions.response_type = 'Short Text'")
+      ::Admin::Question.update_all("admin_questions.response_type = 'Text'", "admin_questions.response_type = 'Long Text'")
+      ::Admin::Question.update_all("admin_questions.response_type = 'Integer'", "admin_questions.response_type = 'Number'")
+    end
   end
 end
