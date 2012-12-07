bb84962defad8f11f2dc82b174b544aa0c8f0582
Hayden Ball <ball.hayden@gmail.com>
2012-12-07 22:10:25 +0000

    Move update_permissions to model

--- app/controllers/admin/permissions_controller.rb
+++ app/controllers/admin/permissions_controller.rb
@@ -27,7 +27,7 @@ class Admin::PermissionsController < AdminController
         actions = _models[model.name] if _models
         actions ||= []
 
-        update_permission(role, model.name, actions)
+        Admin::Permission.update_permission(role, model.name, actions)
       end
 
       other_permissions = ['backend']
@@ -38,43 +38,11 @@ class Admin::PermissionsController < AdminController
         actions = _models[model] if _models
         actions ||= []
 
-        update_permission(role, model, actions)
+        Admin::Permission.update_permission(role, model, actions)
       end
     end
 
     return redirect_to :admin_permissions
-  end
-
-  private
-  def update_permission(role, subject_class, actions)
-    existing_permissions = role.permissions.where({ :subject_class => subject_class })
-
-    if existing_permissions then
-      existing_permissions.each do |perm|
-        if not actions.include? perm.action then
-          #The role no longer has this permission. Get rid
-          role.permissions.delete(perm)
-        end
-      end
-    end
-
-    actions.each do |action|
-      if (not existing_permissions) or (not existing_permissions.find_by_action(action)) then
-        #Try to add the role to the existing permission
-        if permission = Admin::Permission.where({ :action => action, :subject_class => subject_class }).first;
-          permission.roles << role
-        else
-          permission = Admin::Permission.new
-          permission.action = action[0]
-          permission.subject_class = subject_class
-          permission.roles << role
-        end
-
-        permission.save
-      end
-    end
-
-    role.save
   end
 
 end
--- app/models/admin/permission.rb
+++ app/models/admin/permission.rb
@@ -2,4 +2,35 @@ class Admin::Permission < ActiveRecord::Base
   has_and_belongs_to_many :roles
 
   attr_accessible :action, :subject_class
+
+  def self.update_permission(role, subject_class, actions)
+    existing_permissions = role.permissions.where({ :subject_class => subject_class })
+
+    if existing_permissions then
+      existing_permissions.each do |perm|
+        if not actions.include? perm.action then
+          #The role no longer has this permission. Get rid
+          role.permissions.delete(perm)
+        end
+      end
+    end
+
+    actions.each do |action|
+      if (not existing_permissions) or (not existing_permissions.find_by_action(action)) then
+        #Try to add the role to the existing permission
+        if permission = Admin::Permission.where({ :action => action, :subject_class => subject_class }).first;
+          permission.roles << role
+        else
+          permission = Admin::Permission.new
+          permission.action = action[0]
+          permission.subject_class = subject_class
+          permission.roles << role
+        end
+
+        permission.save
+      end
+    end
+
+    role.save
+  end
 end
