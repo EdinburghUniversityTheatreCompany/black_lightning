1231e99211877476d779de6bdf67b9bbf21a9f31
Craig Snowden <craig@craigsnowden.com>
2012-12-16 01:02:01 +0000

    add some form of explanation into techie families. What a horrible mess it is

--- app/controllers/admin/techie_families_controller.rb
+++ app/controllers/admin/techie_families_controller.rb
@@ -1,4 +1,5 @@
 class Admin::TechieFamiliesController < AdminController
+  #The sparse-ness of this controller is the result of apathy on my part. Use the CLI console to add techies and relationships. Maybe someday I'll give it a UI - CS 16/12/12
   def index
     @title = "Techie Families"
     @techies = Techie.all
