836ceab0ad2d16b1739cf88a0e7bac50c72cc925
Hayden Ball <hayden@haydenball.me.uk>
2012-12-27 15:41:29 +0000

    Remove team_member_fields js from all admin pages - only use it when necessary. Fixes #98

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -10,7 +10,6 @@
 // WARNING: THE FIRST BLANK LINE MARKS THE END OF WHAT'S TO BE PROCESSED, ANY BLANK LINE SHOULD
 // GO AFTER THE REQUIRES BELOW.
 //
-//= require admin/team_member_fields
 //= require admin/jquery.gridster.min
 //= require admin/md_editor
 //= require admin/staffings
