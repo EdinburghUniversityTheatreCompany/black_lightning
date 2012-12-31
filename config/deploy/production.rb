4ca4bcf05b46352a360f3fb655e24f87d351a605
Hayden Ball <hayden@haydenball.me.uk>
2012-12-31 12:55:02 +0000

    Require commit to be tagged before deploy to production

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,7 @@
 require "delayed/recipes"
 
+before "deploy", "check_tag"
+
 before "delayed_job:stop",    "deploy:chmoddj"
 before "delayed_job:start",   "deploy:chmoddj"
 before "delayed_job:restart", "deploy:chmoddj"
@@ -10,7 +12,16 @@ after "deploy:restart", "delayed_job:restart"
 
 set :deploy_to, "/var/www/bedlamtheatre_co_uk"
 
-desc "chmod delayed_job script"
-task :chmoddj do
-  run "chmod 775 #{current_path}/script/delayed_job"
+namespace :deploy do
+  desc "chmod delayed_job script"
+  task :chmoddj do
+    run "chmod 775 #{current_path}/script/delayed_job"
+  end
+end
+
+task :check_tag do
+  # Returns false if no tag exists, since git quits with an error.
+  if not system 'git describe --exact-match'
+    raise CommandError.new("Please tag commits before deploying to production.")
+  end
 end
