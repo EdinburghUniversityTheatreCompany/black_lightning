23e9571a00725ee7f8d8e820bf6636b773fe20f1
Hayden Ball <hayden@haydenball.me.uk>
2013-01-03 19:01:40 +0000

    Added more tests. Hit 80% code coverage

---
+++ test/functional/workshops_controller_test.rb
@@ -0,0 +1,20 @@
+require 'test_helper'
+
+class WorkshopsControllerTest < ActionController::TestCase
+
+  test "should get index" do
+    FactoryGirl.create_list(:workshop, 10)
+
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:workshops)
+  end
+
+  test "should get show" do
+    @workshop = FactoryGirl.create(:workshop)
+
+    get :show, id: @workshop
+    assert_response :success
+  end
+
+end
