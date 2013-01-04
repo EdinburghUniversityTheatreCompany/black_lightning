93879b8cf16171672fb64d33aa24ef399d37e12d
Hayden Ball <hayden@haydenball.me.uk>
2013-01-04 12:33:32 +0000

    Mailer tests

--- app/mailers/staffing_mailer.rb
+++ app/mailers/staffing_mailer.rb
@@ -4,6 +4,8 @@ class StaffingMailer < ActionMailer::Base
   def staffing_reminder(job)
     @staffing = job.staffable
     @user = job.user
+    
+    return if @user.nil?
 
     mail(:to => @user.email, :subject => "Bedlam Theatre Staffing")
   end
--- test/functional/proposals_mailer_test.rb
+++ test/functional/proposals_mailer_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class ProposalsMailerTest < ActionMailer::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should send new_proposal" do
+    call = FactoryGirl.create(:proposal_call)
+
+    proposal = FactoryGirl.create(:proposal, call: call)
+    creator = proposal.users.first()
+
+    # Send the email, then test that it got queued
+    proposal.team_members.each do |team_member|
+      email = ProposalsMailer.new_proposal(proposal, creator, team_member).deliver
+      assert !ActionMailer::Base.deliveries.empty?
+
+      # Test the body of the sent email contains what we expect it to
+      assert_equal [team_member.user.email], email.to
+      assert_equal "Bedlam Theatre Proposals - #{proposal.show_title}", email.subject
+    end
+  end
 end
