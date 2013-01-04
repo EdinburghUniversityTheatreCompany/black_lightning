01e4415ba3b6566e3da472ce74c30caffde96b1c
Lewis Eason <lewiseason@gmail.com>
2013-01-04 23:42:15 +0000

    I am a bad person.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@ set :bundle_without, []
 
 set :default_environment, {
     'GEM_HOME' => "/var/www/bedlamtheatre_co_uk/gems",
-    'GEM_PATH' => "/var/www/bedlamtheatre_co_uk/gems"
+    'GEM_PATH' => "/var/www/bedlamtheatre_co_uk/gems",
 }
 
 after "deploy", "deploy:congratulate"
