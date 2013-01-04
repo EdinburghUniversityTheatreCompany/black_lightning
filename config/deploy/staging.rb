ad7bfd542358a7276b2d5f70a9ea7aedfb887ef3
Lewis Eason <lewiseason@gmail.com>
2013-01-04 02:12:54 +0000

    I lied. It does need to know where the gems live. For now.

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -5,8 +5,8 @@ set :bundle_dir, "/var/www/bedlamtheatre_co_uk/gems"
 set :bundle_without, []
 
 set :default_environment, {
-    'GEM_HOME' => "/var/www/bedlamtheatre_co_uk/gems",
-    'GEM_PATH' => "/var/www/bedlamtheatre_co_uk/gems"
+    'GEM_HOME' => "/usr/local/rvm/gems/ruby-1.9.3-p362@global",
+    'GEM_PATH' => "/usr/local/rvm/gems/ruby-1.9.3-p362@global"
 }
 
 after "deploy", "deploy:congratulate"
