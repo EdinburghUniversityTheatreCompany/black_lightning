dddc2ff26d9d9ee5d9822b19c1548b9b0d607f31
Hayden Ball <hayden@haydenball.me.uk>
2013-01-05 11:25:54 +0000

    Added link_to user from show page, and corrected event lookup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
     @user = User.where({:public_profile => true}).find(params[:id])
     @title = @user.name
 
+    # Note this uses the ARRAY method "select" to filter results. (Since
+    # teamwork is a polymorphic association, you can't just join the
+    # events table and filter that).
+    @shows = @user.team_membership.where({ teamwork_type: "Event" }).select { |e| e.teamwork.is_a? Show }
+
     respond_to do |format|
       format.html # show.html.erb
     end
