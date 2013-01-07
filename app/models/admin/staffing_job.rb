d30bd0591ce81822207535d10ca6d3bc703a94a9
Hayden Ball <hayden@haydenball.me.uk>
2013-01-07 13:53:30 +0000

    Some tidying up, and fixed issues with saving new staffing / staffing template
    Conflicts:
    	app/models/admin/staffing.rb
    	app/models/admin/staffing_job.rb

--- app/models/admin/staffing_job.rb
+++ app/models/admin/staffing_job.rb
@@ -1,3 +1,6 @@
+##
+# Represents a job/position that may need to be staffed.
+#
 # == Schema Information
 #
 # Table name: admin_staffing_jobs
@@ -9,23 +12,6 @@
 # *created_at*::     <tt>datetime, not null</tt>
 # *updated_at*::     <tt>datetime, not null</tt>
 # *staffable_type*:: <tt>string(255)</tt>
-#--
-# == Schema Information End
-#++
-
-##
-# Represents a job/position that may need to be staffed.
-#
-# == Schema Information
-#
-# Table name: admin_staffing_jobs
-#
-# *id*::          <tt>integer, not null, primary key</tt>
-# *name*::        <tt>string(255)</tt>
-# *staffing_id*:: <tt>integer</tt>
-# *user_id*::     <tt>integer</tt>
-# *created_at*::  <tt>datetime, not null</tt>
-# *updated_at*::  <tt>datetime, not null</tt>
 #--
 # == Schema Information End
 #++
@@ -51,5 +37,4 @@ class Admin::StaffingJob < ActiveRecord::Base
   def js_end_time
     return staffable.end_time.to_i
   end
-
 end
