ac2a0ac2bbdabe03978f9e50ffd1abd337ab721d
Hayden Ball <hayden@haydenball.me.uk>
2013-01-08 11:39:08 +0000

    Only show public shows in user profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,7 +15,7 @@ class UsersController < ApplicationController
     # Note this uses the ARRAY method "select" to filter results. (Since
     # teamwork is a polymorphic association, you can't just join the
     # events table and filter that).
-    @shows = @user.team_membership.where({ teamwork_type: "Event" }).select { |e| e.teamwork.is_a? Show }
+    @shows = @user.team_membership.where({ teamwork_type: "Event" }).select { |e| (e.teamwork.is_a? Show) && (e.teamwork.is_public) }
 
     respond_to do |format|
       format.html # show.html.erb
