48292a9acad460ea02f98ba650b7496d6f2c99ad
Hayden Ball <hayden@haydenball.me.uk>
2013-01-10 15:07:29 +0000

    Should fix problem with seasons if season not found

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -1,10 +1,5 @@
 class SeasonsController < ApplicationController
   def show
-    if request.subdomain == 'www' || request.subdomain == '' then
-      @season = Season.find_by_slug(params[:id])
-    else
-      Rails.logger.debug request.subdomain
-      @season = Season.find_by_slug(request.subdomain)
-    end
+    @season = Season.find_by_slug!(params[:id])
   end
 end
