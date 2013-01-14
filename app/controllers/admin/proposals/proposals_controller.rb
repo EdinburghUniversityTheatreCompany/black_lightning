c31a0a07e4efb360acad04abc93a3a03053e0f98
Hayden Ball <hayden@haydenball.me.uk>
2013-01-14 21:03:10 +0000

    Revert "People can't see proposals." Those are actually correct and need to be there.
    This reverts commit 6b1cbe652f22ded56eec433712d18a783c26cdc5.
    
    Conflicts:
    	app/controllers/admin/proposals/proposals_controller.rb

--- app/controllers/admin/proposals/proposals_controller.rb
+++ app/controllers/admin/proposals/proposals_controller.rb
@@ -61,9 +61,9 @@ class Admin::Proposals::ProposalsController < AdminController
 
     authorize!(:read, @admin_proposals_proposal)
 
-    # Dude, what the hell?
-    #  @admin_proposals_proposal.update_answers
-    #  @admin_proposals_proposal.save
+    # This may look odd, but it is fine. Honest. (HB Jan 13)
+    @admin_proposals_proposal.update_answers
+    @admin_proposals_proposal.save
 
     respond_to do |format|
       format.html # show.html.erb
@@ -99,7 +99,7 @@ class Admin::Proposals::ProposalsController < AdminController
       @proposal.late = true
     end
 
-    @proposal.answers
+    @proposal.update_answers
 
     respond_to do |format|
       format.html # new.html.erb
