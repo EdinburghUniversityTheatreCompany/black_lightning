61dd27e390b5db15f3cacbd5d15ac0ebaa71dac1
Hayden Ball <hayden@haydenball.me.uk>
2013-01-25 16:05:44 +0000

    Corrected widget, added validation for expiry date

--- app/models/opportunity.rb
+++ app/models/opportunity.rb
@@ -4,5 +4,7 @@ class Opportunity < ActiveRecord::Base
 
   attr_accessible :approved, :approver_id, :creator_id, :description, :show_email, :title, :expiry_date
 
+  validates :expiry_date, :presence => true
+
   scope :approved, where("approved = true AND expiry_date > ? ", Time.now )
 end
--- test/factories/opportunity_factory.rb
+++ test/factories/opportunity_factory.rb
@@ -3,6 +3,8 @@ FactoryGirl.define do
      title        { generate(:random_string) }
      description  { generate(:random_text) }
      show_email   { [true, false].sample }
+     expiry_date  { 5.days.from_now }
+
      association :creator, factory: :user
   end
 end
