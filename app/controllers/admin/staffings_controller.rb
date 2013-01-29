913cacd43c5a1fbec78c64b69c8e06fc1f2218b8
Hayden Ball <hayden@haydenball.me.uk>
2013-01-29 11:20:30 +0000

    Should fix problem with creating staffing for show

--- app/controllers/admin/staffings_controller.rb
+++ app/controllers/admin/staffings_controller.rb
@@ -102,21 +102,24 @@ class Admin::StaffingsController < AdminController
     start_times = params[:start_times]
     end_times = params[:end_times]
 
-    start_times.values.zip(end_times.values).each do |start_time, end_time|
-      staffing = admin_staffing.dup
+    begin
+      start_times.values.zip(end_times.values).each do |start_time, end_time|
+        staffing = admin_staffing.dup
 
-      staffing.start_time = DateTime.civil(start_time[:year].to_i, start_time[:month].to_i, start_time[:day].to_i, start_time[:hour].to_i, start_time[:minute].to_i)
-      staffing.end_time = DateTime.civil(start_time[:year].to_i, start_time[:month].to_i, start_time[:day].to_i, end_time[:hour].to_i, end_time[:minute].to_i) # right now I'm assuming staffings end on the same day as they begin. Makes the UI cleaner
-      if not staffing.save
-        redirect_to redirect_to admin_staffings_url, alert: 'There were errors creating the staffing.'
-        return
+        staffing.start_time = DateTime.civil(start_time[:year].to_i, start_time[:month].to_i, start_time[:day].to_i, start_time[:hour].to_i, start_time[:minute].to_i)
+        staffing.end_time = DateTime.civil(start_time[:year].to_i, start_time[:month].to_i, start_time[:day].to_i, end_time[:hour].to_i, end_time[:minute].to_i) # right now I'm assuming staffings end on the same day as they begin. Makes the UI cleaner
+
+        staffing.save!
+
+        staffing.staffing_jobs << admin_staffing_jobs.collect { |job| job.dup }
       end
-
-      staffing.staffing_jobs << admin_staffing_jobs.collect { |job| job.dup }
+    rescue => e
+      redirect_to redirect_to admin_staffings_path, alert: 'There were errors creating the staffing.'
+      return
     end
 
     flash[:success] = 'Staffing was successfully created.'
-    redirect_to admin_staffings_url
+    redirect_to admin_staffings_path
   end
 
   ##
