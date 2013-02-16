f108395544287232960df9a827ca6961614f6554
Hayden Ball <hayden@haydenball.me.uk>
2013-02-16 15:26:07 +0000

    Fix tests

--- test/functional/archives/shows_controller_test.rb
+++ test/functional/archives/shows_controller_test.rb
@@ -3,11 +3,6 @@ require 'test_helper'
 class Archives::ShowsControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
-    assert_redirected_to archives_shows_index_path(01, 1.years.ago.year, 12, Date.today.year)
-  end
-
-  test "should get index with date" do
-    get :index, { :start_month => 1, :start_year => 1.years.ago.year, :end_month => 12, :end_year => Date.today.year }
     assert_response :success
   end
 end
--- test/functional/archives_controller_test.rb
+++ test/functional/archives_controller_test.rb
@@ -7,9 +7,4 @@ class ArchivesControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  test "should get index with date" do
-    get :index, { :start_month => 01, :start_year => 1.years.ago.year, :end_month => 12, :end_year => Date.today.year }
-    assert_response :success
-  end
-
 end
