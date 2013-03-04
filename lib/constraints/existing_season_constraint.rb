7b42d93390186398bbfd49f90b5cdfd358f60f66
Hayden Ball <hayden@haydenball.me.uk>
2013-03-04 01:05:28 +0000

    Check table exists before applying constraint. Fixes #123

--- lib/constraints/existing_season_constraint.rb
+++ lib/constraints/existing_season_constraint.rb
@@ -1,9 +1,15 @@
 class ExistingSeasonConstraint
   def initialize
-    @season_slugs = Season.select(:slug).collect { |s| s.slug }
+    if Season.table_exists?
+      @season_slugs = Season.select(:slug).collect { |s| s.slug }
+    else
+      @season_slugs = nil
+    end
   end
 
   def matches?(request)
+    return false if @season_slugs.nil?
+
     @season_slugs.include?(request.url.split('/').last)
   end
 end
