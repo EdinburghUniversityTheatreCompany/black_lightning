29fc020076f3bb9f3d1486f8aa7e0ed9350dd55f
Craig Snowden <craig@craigsnowden.com>
2013-03-22 00:53:21 +0000

    add token authentication for future API goodness

--- app/models/user.rb
+++ app/models/user.rb
@@ -38,7 +38,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable
+  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :token_authenticatable
 
   # set our own validations
 
