435c0dbd097f268921276927c203dfa88c1b8c58
Craig Snowden <craig@craigsnowden.com>
2013-11-12 15:37:49 +0000

    fix multiple xts widgets on one page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,8 @@
 module ApplicationHelper
   def xts_widget(xts_id)
-    "<div id='ticketslist' class='xtsprodates'></div>
+    "<div id='tickets-#{xts_id}' class='xtsprodates'></div>
 <script src='http://www.xtspro.com/book/book.js'></script>
-<script>XTSPRO.insert_dates('BT', #{xts_id}, '#ticketslist');</script>".html_safe
+<script>XTSPRO.insert_dates('BT', #{xts_id}, '#tickets-#{xts_id}');</script>".html_safe
   end
   def strip_tags(html)
       return self.gsub(%r{</?[^>]+?>}, '')
