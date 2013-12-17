23c787f60d3ecd4763948ed4d3d86de6501967fe
Hayden Ball <hayden@haydenball.me.uk>
2013-12-17 12:01:25 +0000

    Added X-Total-Count header

--- app/controllers/archives/shows_controller.rb
+++ app/controllers/archives/shows_controller.rb
@@ -2,8 +2,11 @@ class Archives::ShowsController < ArchivesController
   def index
     @q = Show.search(params[:q])
     @shows = @q.result(distinct: true)
-               .paginate(:page => params[:page], :per_page => 5)
                .where(:is_public => true)
+
+    response.headers["X-Total-Count"] = @shows.count.to_s
+
+    @shows = @shows.paginate(:page => params[:page], :per_page => 5)
 
     respond_to do |format|
       format.html # index.html.erb
