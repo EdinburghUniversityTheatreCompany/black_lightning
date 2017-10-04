c11a42f8c72a943f86116ef7401b65a2393f57ad
Kyle Cooke <cookekyle99@yahoo.co.uk>
2017-10-04 10:59:51 +0100

    rewriting delayed job script to new server structure

--- script/monit_delayed_job.sh
+++ script/monit_delayed_job.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $# -lt 1 ] ; then
-    echo "Usage:   " $0 " <start | stop> "
+    echo "Usage:   " $0 " <start | stop | restart> "
     exit 1
 fi
 
@@ -9,10 +9,10 @@ action=$1
 
 export PATH=/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
 export RAILS_ENV=production
-export GEM_PATH="/var/www/bedlamtheatre_co_uk/gems"
-export GEM_HOME="/var/www/bedlamtheatre_co_uk/gems"
+export GEM_PATH="/srv/black_lightning/current/gems"
+export GEM_HOME="/srv/black_lightning/current/gems"
 
-cd /var/www/bedlamtheatre_co_uk/current
+cd /srv/black_lightning/current
 
-/var/www/bedlamtheatre_co_uk/gems/bin/bundle exec /var/www/bedlamtheatre_co_uk/current/script/delayed_job $action
+bundle exec script/delayed_job $action
 
