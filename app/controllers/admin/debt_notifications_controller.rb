ca91acd3d1905dd2fb2db5fc036239e5110defe4
Kyle Cooke <cookekyle99@yahoo.co.uk>
2017-10-12 15:25:10 +0100

    preventing manipulation of notification records

--- app/controllers/admin/debt_notifications_controller.rb
+++ app/controllers/admin/debt_notifications_controller.rb
@@ -17,37 +17,27 @@ class Admin::DebtNotificationsController < AdminController
 
   # GET /admin/debt_notifications/new
   def new
-    @admin_debt_notification = Admin::DebtNotification.new
+    redirect_to admin_debt_notifications_path, notice:"Debt notifications shouldn't be created manually"
   end
 
   # GET /admin/debt_notifications/1/edit
   def edit
+    redirect_to admin_debt_notifications_path, notice:"Debt notifications shouldn't be modified"
   end
 
   # POST /admin/debt_notifications
   def create
-    @admin_debt_notification = Admin::DebtNotification.new(admin_debt_notification_params)
-
-    if @admin_debt_notification.save
-      redirect_to @admin_debt_notification, notice: 'Debt notification was successfully created.'
-    else
-      render :new
-    end
+    redirect_to admin_debt_notifications_path, notice:"Debt notifications shouldn't be created manually"
   end
 
   # PATCH/PUT /admin/debt_notifications/1
   def update
-    if @admin_debt_notification.update(admin_debt_notification_params)
-      redirect_to @admin_debt_notification, notice: 'Debt notification was successfully updated.'
-    else
-      render :edit
-    end
+    redirect_to admin_debt_notifications_path, notice:"Debt notifications shouldn't be modified"
   end
 
   # DELETE /admin/debt_notifications/1
   def destroy
-    @admin_debt_notification.destroy
-    redirect_to admin_debt_notifications_url, notice: 'Debt notification was successfully destroyed.'
+    redirect_to admin_debt_notifications_path, notice:"Debt notifications shouldn't be destroyed"
   end
 
   private
