1127ae4a6d566e2da5450b99d703b70c42d03162
Kyle Cooke <cookekyle99@yahoo.co.uk>
2017-10-13 08:16:27 +0100

    re-writing debt_notifications_controller_test to stop checking destroy and update as they shouldn't be possible and checking new and edit give redirects

--- test/controllers/admin/debt_notifications_controller_test.rb
+++ test/controllers/admin/debt_notifications_controller_test.rb
@@ -2,48 +2,31 @@ require 'test_helper'
 
 class Admin::DebtNotificationsControllerTest < ActionController::TestCase
   setup do
-    @admin_debt_notification = admin_debt_notifications(:one)
+    @admin_debt_notification = FactoryGirl.create(:initial_debt_notification)
+    sign_in FactoryGirl.create(:admin)
   end
 
   test "should get index" do
     get :index
     assert_response :success
-    assert_not_nil assigns(:admin_debt_notifications)
+    assert_not_nil assigns(:debt_notifications)
   end
 
-  test "should get new" do
+  test "should not get new" do
     get :new
-    assert_response :success
+    assert_redirected_to admin_debt_notifications_url
   end
 
-  test "should create admin_debt_notification" do
-    assert_difference('Admin::DebtNotification.count') do
-      post :create, admin_debt_notification: { sent_on: @admin_debt_notification.sent_on, user_id: @admin_debt_notification.user_id }
-    end
-
-    assert_redirected_to admin_debt_notification_path(assigns(:admin_debt_notification))
-  end
 
   test "should show admin_debt_notification" do
     get :show, id: @admin_debt_notification
     assert_response :success
   end
 
-  test "should get edit" do
+  test "should not get edit" do
     get :edit, id: @admin_debt_notification
-    assert_response :success
+    assert_redirected_to admin_debt_notifications_url
   end
 
-  test "should update admin_debt_notification" do
-    patch :update, id: @admin_debt_notification, admin_debt_notification: { sent_on: @admin_debt_notification.sent_on, user_id: @admin_debt_notification.user_id }
-    assert_redirected_to admin_debt_notification_path(assigns(:admin_debt_notification))
-  end
 
-  test "should destroy admin_debt_notification" do
-    assert_difference('Admin::DebtNotification.count', -1) do
-      delete :destroy, id: @admin_debt_notification
-    end
-
-    assert_redirected_to admin_debt_notifications_path
-  end
 end
