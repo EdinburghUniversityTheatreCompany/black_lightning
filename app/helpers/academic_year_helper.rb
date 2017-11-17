b0a32a86f45a4121578e65e994b25d4ecf5e0ecb
Kyle Cooke <cookekyle99@yahoo.co.uk>
2017-11-17 15:10:06 +0000

    adding a widget for staffing manager to see how many staffing jobs committee have done in each semester.

---
+++ app/helpers/academic_year_helper.rb
@@ -0,0 +1,21 @@
+module AcademicYearHelper
+  def start_of_year
+    d1 = Date.new(Date.current.year, 9, 1)
+    return d1 if d1.past?
+    return Date.new(Date.current.year - 1, 9, 1)
+  end
+
+  def christmas
+    if Date.current.month >= 9
+      Date.new(Date.current.year, 12, 25)
+    else
+      Date.new(Date.current.year - 1, 12, 25)
+    end
+  end
+
+  def next_year_start
+    d1 = Date.new(Date.current.year, 9, 1)
+    return d1 unless d1.past?
+    return Date.new(Date.current.year + 1, 9, 1)
+  end
+end
