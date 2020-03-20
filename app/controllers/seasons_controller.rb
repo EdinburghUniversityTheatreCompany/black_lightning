4a1da6a1b81167216c6cfd94eb29dba270fb6438
Mick Zijdel <mickzijdel@hotmail.com>
2020-03-20 11:51:10 +0100

    Group events on season show by day Turn the header of an event on the season show page into a link to the event Add some headers to the season show page Show date(s) on the season show page Simplify _event_label because you can also just generate a link for the event

--- app/controllers/seasons_controller.rb
+++ app/controllers/seasons_controller.rb
@@ -2,5 +2,6 @@ class SeasonsController < ApplicationController
   # GET /seasons/1
   def show
     @season = Season.find_by_slug!(params[:id])
+    @events = @season.events.order(:start_date).group_by {|event| l event.start_date, format: :longy }
   end
 end
