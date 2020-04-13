3f17ad76a0b85604be2713e7a1ce8a40d1666837
Mick Zijdel <mickzijdel@hotmail.com>
2020-04-13 19:01:08 +0200

    Add initialize_urls line to the slug migration

--- db/migrate/20200320112531_add_slug_to_admin_staffings.rb
+++ db/migrate/20200320112531_add_slug_to_admin_staffings.rb
@@ -2,5 +2,9 @@ class AddSlugToAdminStaffings < ActiveRecord::Migration
   def change
     add_column :admin_staffings, :slug, :string
     add_index :admin_staffings, :slug
+
+    reversible do |direction|
+      direction.up { Admin::Staffing.initialize_urls }
+    end
   end
 end
