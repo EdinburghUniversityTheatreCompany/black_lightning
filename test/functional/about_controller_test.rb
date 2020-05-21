a2b54e54beff294fa0622ea7019a9b213851090a
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-21 19:34:57 +0200

    Use generic subpages things fo the about controller Add tests to the about controller

--- test/functional/about_controller_test.rb
+++ test/functional/about_controller_test.rb
@@ -1,10 +1,22 @@
 require 'test_helper'
 
 class AboutControllerTest < ActionController::TestCase
+  include SubpageHelper
   # Index is a special case and explicitly routed to correspond to /about/.
   test 'should get index' do
     get :index
-    assert_response :success
+    check_if_overview
+  end
+
+  test 'should get index through page' do
+    get :page
+    check_if_overview
+    get :page, params: { page: nil }
+    check_if_overview
+    get :page, params: { page: '' }
+    check_if_overview
+    get :page, params: { page: 'overview' }
+    check_if_overview
   end
 
   # Test if getting an existing page works.
@@ -17,5 +29,14 @@ class AboutControllerTest < ActionController::TestCase
   test 'should not get non-existent page' do
     get :page, params: { page: 'this/page/does/not/exist/I/think' }
     assert_response :missing
+  end
+
+  private
+
+  def check_if_overview
+    assert_response :success
+
+    assert_equal '', assigns(:root_page)
+    assert_equal get_subpages('about', ''), assigns(:subpages)
   end
 end
