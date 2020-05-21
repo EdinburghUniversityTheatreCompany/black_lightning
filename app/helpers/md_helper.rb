c0b1cb6b35531e76818f3234c44c11202d025620
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-21 15:00:48 +0200

    Test MD helper

--- app/helpers/md_helper.rb
+++ app/helpers/md_helper.rb
@@ -2,17 +2,14 @@ require "#{Rails.root}/lib/kramdown/parser/b_kramdown"
 
 module MdHelper
   def render_markdown(md)
-    if md.nil?
-      return ''
-    end
+    return '' if md.nil?
 
     return Kramdown::Document.new(md, input: 'BKramdown').to_html.html_safe
   end
 
   def render_plain(md)
-    if md.nil?
-      return ''
-end
-    return Kramdown::Document.new(md, input: 'BKramdown').to_html.gsub(%r{</?[^>]+?>}, '')
+    return '' if md.nil?
+
+    return strip_tags(Kramdown::Document.new(md, input: 'BKramdown').to_html)
   end
 end
