d59a8a71b9f411328bfc365e5fd2e9503c7bad24
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-21 17:16:00 +0200

    Add tests for unchanged mailers

---
+++ test/functional/mailers/reports_mailer_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class ReportsMailerTest < ActionMailer::TestCase
+  test 'send report' do
+    report = RolesReport.new
+    user = FactoryBot.create(:user)
+
+    mail = nil
+
+    assert_difference 'ActionMailer::Base.deliveries.count' do
+      mail = ReportsMailer.send_report(user, report).deliver_now
+    end
+
+    assert_equal [user.email], mail.to
+    assert_equal 'Bedlam Theatre Report', mail.subject
+  end
+end
