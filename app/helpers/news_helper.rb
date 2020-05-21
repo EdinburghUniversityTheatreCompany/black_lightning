5fe945ae5841e74128db8090f960ff14a09e183c
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-21 15:08:21 +0200

    Test and update the news hleper

--- app/helpers/news_helper.rb
+++ app/helpers/news_helper.rb
@@ -2,7 +2,22 @@ module NewsHelper
   # find the first line break after 140 characters
   def generate_preview(content)
     begin
-      return /.{,140}.+?<\/p>/m.match(content)[0]
+      preview = ''
+
+      while content.present?
+        partition = content.partition(/(\n)|(<\/p>)/)
+
+        preview = preview.concat(partition.first)
+
+        if preview.length < 140 && partition[2].present?
+          preview = preview.concat(partition[1])
+          content = partition[2]
+        else
+          break
+        end
+      end
+
+      return preview
     rescue
       # :nocov:
       return 'There was an error rendering a preview for this news item.'
