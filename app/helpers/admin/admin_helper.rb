c9fa01c46b78d2df7ca7c2b085a45c76ed6080d7
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-21 15:33:20 +0200

    Add the correct version of the admin helper + tests

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,11 +1,10 @@
 module Admin::AdminHelper
-  
   # Destroys the passed object and adds a success or error message to the flash.
   # Checks if the current_ability can destroy the object. Also tests with an and for the additional condition.
   # When condition is specified, it does not check for destroy permission and the additional permission.
   # Raises an ArgumentError if the object passed is nil.
   # Raises a TypeError if the object passed is a class, and not an instance.
-  def destroy_with_flash_message(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_message: true)
+  def destroy_with_flash_message(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_flash: true)
     raise(ArgumentError, 'The object is nil') if object.nil?
     raise(TypeError, "#{object} is a class and not an instance so it cannot be destroyed.") if object.is_a?(Class)
 
@@ -19,19 +18,22 @@ module Admin::AdminHelper
     name ||= "The #{get_formatted_class_name(object)}#{" \"#{object_name}\"" if object_name.present?}"
 
     if object.destroy
-      success_message ||= "#{name} has been successfully destroyed."
-      flash[:success] = success_message
+      append_to_flash(:success, success_message || "#{name} has been successfully destroyed.")
+
       return true
     else
-      error_message ||= "#{name} could not be destroyed."
-      error_message +=  " #{object.errors.messages[:destroy].join('. ')}" if append_errors_to_error_message
+      append_to_flash(:error, error_message || "#{name} could not be destroyed.")
+      if append_errors_to_error_flash
+        object.errors.messages[:destroy].each do |message|
+          append_to_flash(:error, message)
+        end
+      end
 
-      flash[:error] = error_message
       return false
     end
   end
 
-  def destroy_with_flash_message!(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_message: true)
-    raise(ActiveRecord::RecordNotDestroyed, flash[:error]) unless destroy_with_flash_message(object, condition: condition, additional_condition: additional_condition, name: name, success_message: success_message, error_message: error_message, append_errors_to_error_message: append_errors_to_error_message)
+  def destroy_with_flash_message!(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_flash: true)
+    raise(ActiveRecord::RecordNotDestroyed, flash[:error]) unless destroy_with_flash_message(object, condition: condition, additional_condition: additional_condition, name: name, success_message: success_message, error_message: error_message, append_errors_to_error_flash: append_errors_to_error_flash)
   end
 end
