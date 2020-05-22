216bf1a2bd4695e4a017855e21ec9b91f3ce2a66
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-22 10:12:01 +0200

    Debt Clean up a little Add tests

---
+++ test/models/admin/debt_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class Admin::EditableBlockTest < ActiveSupport::TestCase
+  setup do
+    @user = FactoryBot.create :user
+  end
+
+  test 'create debt' do
+    debt = Admin::Debt.new(@user.id)
+    assert_equal @user.id, debt.id
+  end
+
+  test 'get oldest debt' do
+    FactoryBot.create :maintenance_debt, user: @user, due_by: Date.today.advance(days: -15)
+    FactoryBot.create :staffing_debt, user: @user, due_by: Date.today.advance(days: -5)
+
+    assert_equal Date.today.advance(days: -15), Admin::Debt.users_oldest_debt(@user.id)
+  end
+end
