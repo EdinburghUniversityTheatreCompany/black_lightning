d0624aa4eb165c63f578f2275060fb0b2d513faa
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-22 12:15:09 +0200

    More improvements to controllers I did not include earlier

--- app/controllers/admin/permissions_controller.rb
+++ app/controllers/admin/permissions_controller.rb
@@ -2,30 +2,22 @@
 # The controller for setting permissions.
 ##
 class Admin::PermissionsController < AdminController
+  authorize_resource
+  before_action :set_models_and_roles
   ##
   # Shows a grid for selecting permissions for each role.
   ##
   def grid
     @title = 'Permissions'
-    authorize! :read, Admin::Permission
+    @models.sort_by!(&:name)
 
     @actions = %w[read create update delete manage]
-    @roles = get_roles
-
-    Rails.application.eager_load!
-    @models = get_models
   end
 
   ##
   # Takes the data posted from the grid and sets the permissions.
   ##
   def update_grid
-    authorize! :manage, Admin::Permission
-    @roles = get_roles
-
-    Rails.application.eager_load!
-    @models = get_models
-
     @roles.each do |role|
       models = params[role.name]
 
@@ -40,19 +32,17 @@ class Admin::PermissionsController < AdminController
     return redirect_to :admin_permissions
   end
 
-  def get_roles
-    role_exclude = ['admin']
-    return ::Role.includes(:permissions).where('name NOT IN (?)', role_exclude).all
-  end
-
-  def get_models
+  def set_models_and_roles
     @miscellaneous_permission_subject_classes = {
       'Admin::StaffingJob' => { 'sign_up_for' => 'Sign Up For Staffing' },
       'backend' => { 'access' => 'Access Backend' },
-      'reports' => { 'read' => 'Read Reports' }
+      'reports' => { 'read' => 'Read Reports' },
+      'User' => { 'view_public_profile' => 'View the public part of the user profile (Bio, avatar, and shows)' },
     }
 
-    models = ApplicationRecord.descendants + [Admin::Debt]
-    return models.sort_by(&:name)
+    @models = ApplicationRecord.descendants + [Admin::Debt, Season] - [Event]
+
+    role_exclude = ['admin']
+    @roles = Role.includes(:permissions).where.not(name: role_exclude).all
   end
 end
