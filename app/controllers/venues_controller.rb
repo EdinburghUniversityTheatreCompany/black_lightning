5ff3c402abc819a41532b58294cdab7f91c610c6
Mick Zijdel <mickzijdel@hotmail.com>
2020-05-22 12:12:42 +0200

    Improvements to controllers that I did not include earlier

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,14 +2,16 @@
 # Public controller for Venue. More details can be found there.
 ##
 class VenuesController < ApplicationController
+  load_and_authorize_resource
   ##
   # GET /venues
   #
   # GET /venues.json
   ##
   def index
-    @venues = Venue.paginate(page: params[:page], per_page: 5).all
     @title = 'Venues'
+    @venues = @venues.paginate(page: params[:page], per_page: 20)
+
     respond_to do |format|
       format.html # index.html.erb
       format.json { render json: @venues }
@@ -22,7 +24,6 @@ class VenuesController < ApplicationController
   # GET /venues/1.json
   ##
   def show
-    @venue = Venue.find(params[:id])
     @title = @venue.name
 
     @current_shows = @venue.shows.current.first(3)
