28215f725ed6dc787db74e39d87969a706758a5e
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-03 22:35:59 +0200

    Fix error when searching for user names in proposals

--- app/controllers/archives/proposals_controller.rb
+++ app/controllers/archives/proposals_controller.rb
@@ -4,9 +4,19 @@ class Archives::ProposalsController < AdminController
 
     @title = 'Proposal Archive'
 
+    # The next chapter in hacky solutions to accessible by errors.
+    # Because accessible_by and the search both need the team_members table, it errors when you search for a person name.
+
     @q = Admin::Proposals::Proposal.ransack(params[:q])
-    @proposals = @q.result(distinct: true).accessible_by(current_ability)
-    @calls = Admin::Proposals::Call.where(id: @proposals.reverse.collect(&:call_id)).paginate(page: params[:page], per_page: 20)
-    @proposals = @proposals.where(call_id: @calls.collect(&:id)).includes(:call, :team_members, :users).group_by(&:call)
+
+    @proposals = @q.result(distinct: true)
+    result_ids = @proposals.ids
+
+    @proposals = Admin::Proposals::Proposal.where(id: result_ids).accessible_by(current_ability)
+    proposal_ids = @proposals.reverse.collect(&:call_id)
+
+    @calls = Admin::Proposals::Call.where(id: proposal_ids).paginate(page: params[:page], per_page: 20)
+
+    @proposals = @proposals.where(call_id: @calls.collect(&:id)).includes(:call, team_members: [user: [:admin_maintenance_debts, :admin_staffing_debts]]).group_by(&:call)
   end
 end
