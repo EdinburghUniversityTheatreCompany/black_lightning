45d3eee4c53eaaeef046d844c985cdd1419e9473
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-12 10:05:51 +0200

    Add tests for the active_storage_helper Raise an ArgumentError if the specified default file does not exist Use a slash in the default name prefix so user uploaded files will very likely not match

--- app/helpers/active_storage_helper.rb
+++ app/helpers/active_storage_helper.rb
@@ -2,15 +2,23 @@ module ActiveStorageHelper
   # Please change the note in the assets/images/defaults folder if you change this.
   def default_image_blob(default_image_filename)
     prefix = 'active_storage_default'.freeze
-    key_filename = "#{prefix}-#{default_image_filename}"
+
+    # Use a slash so user uploaded files are very unlikely to match.
+    # It's not an issue if they do match, but it makes life harder when you want to replace the default image.
+    key_filename = "#{prefix}/#{default_image_filename}"
+
     blob = ActiveStorage::Blob.find_by(filename: key_filename)
 
     if blob.nil?
-      blob = ActiveStorage::Blob.create_after_upload!(
-        io: File.open(Rails.root.join('app', 'assets', 'images', 'defaults', default_image_filename)),
-        filename: key_filename, 
-        content_type: 'image/png'  
-      )
+      begin
+        blob = ActiveStorage::Blob.create_after_upload!(
+          io: File.open(Rails.root.join('app', 'assets', 'images', 'defaults', default_image_filename)),
+          filename: key_filename, 
+          content_type: 'image/png'  
+        )
+      rescue Errno::ENOENT => e
+        raise ArgumentError, "There is no default file named #{default_image_filename} in the assets/images/defaults folder"
+      end
     end
     
     return blob
--- test/helpers/active_storage_helper_test.rb
+++ test/helpers/active_storage_helper_test.rb
@@ -1,11 +1,42 @@
 require 'test_helper'
 
 class ActiveStorageHelperTest < ActionView::TestCase
+  test 'default_image_blob' do 
+    blob = nil
+    assert_no_difference('ActiveStorage::Attachment.count') do
+      assert_difference('ActiveStorage::Blob.count') do
+        blob = default_image_blob('bedlam.png')
+      end
+    end
+
+    assert blob.filename = 'active_storage_default_bedlam.png'
+
+    assert_no_difference('ActiveStorage::Attachment.count') do
+      assert_no_difference('ActiveStorage::Blob.count') do
+        other_blob = default_image_blob('bedlam.png')
+
+        assert_equal blob, other_blob
+      end
+    end
+  end
+
+  test 'default_image_blob with invalid image filename' do
+    assert_raises ArgumentError do
+      assert_no_difference('ActiveStorage::Attachment.count') do
+        assert_no_difference('ActiveStorage::Blob.count') do
+          default_image_blob('pineapple.png')
+        end
+      end
+    end
+  end
+
   test 'slideshow_variant' do
     assert slideshow_variant.is_a? Hash
+    assert slideshow_variant.values.first.is_a? Array
   end
 
   test 'thumb_variant' do
-    slideshow_variant.is_a? Hash
+    assert thumb_variant.is_a? Hash
+    assert thumb_variant.values.first.is_a? Array
   end
 end
