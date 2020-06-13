2030a120f9ddaef7506cd17f20e29aea68bfe742
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-13 09:15:31 +0200

    Add Google analytics to allowed CORS origins

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 ChaosRails::Application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins  'localhost:3000', '127.0.0.1:3000', '/(.+?)\.bedlamtheatre\.co\.uk', 'bedlamtheatre.co.uk', 'api.twitter.com'
+    origins  'localhost:3000', '127.0.0.1:3000', '/(.+?)\.bedlamtheatre\.co\.uk', 'bedlamtheatre.co.uk', 'api.twitter.com', 'www.google-analytics.com', 'ssl.google-analytics.com'
     resource '*',
              headers: :any,
              methods: [:get, :post, :put, :patch, :delete, :options, :head]
