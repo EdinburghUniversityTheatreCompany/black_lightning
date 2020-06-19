528eb56452b25b01233e6d3150a73feda5d06199
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-19 10:37:17 +0200

    Update destroy_with_with_flash_message to give better error messages when destroying fails Update the test for destroy_with_flash_message by adding a current_ability to the admin_helper_test

--- app/helpers/admin/admin_helper.rb
+++ app/helpers/admin/admin_helper.rb
@@ -1,28 +1,37 @@
 module Admin::AdminHelper
+  include NameHelper
+
   # Destroys the passed object and adds a success or error message to the flash.
-  # Checks if the current_ability can destroy the object. Also tests with an and for the additional condition.
+  # Checks if the ability can destroy the object. Also tests with an and for the additional condition.
   # When condition is specified, it does not check for destroy permission and the additional permission.
   # Raises an ArgumentError if the object passed is nil.
   # Raises a TypeError if the object passed is a class, and not an instance.
-  def destroy_with_flash_message(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_flash: true)
+  def destroy_with_flash_message(object, condition: nil, additional_condition: true, name: nil, success_message: nil, error_message: nil, append_errors_to_error_flash: true, ability: current_ability)
     raise(ArgumentError, 'The object is nil') if object.nil?
     raise(TypeError, "#{object} is a class and not an instance so it cannot be destroyed.") if object.is_a?(Class)
-
-    condition = can?(:destroy, object) && additional_condition if condition.nil?
-
-    return unless condition
 
     # This code gets the name if there is no name specified.
     # Examples: The Maintenance Debt, The Venue Bedlam Theatre
     object_name = get_object_name(object, '')
-    name ||= "The #{get_formatted_class_name(object)}#{" \"#{object_name}\"" if object_name.present?}"
+    name ||= "the #{get_formatted_class_name(object)}#{" \"#{object_name}\"" if object_name.present?}"
+
+    condition = ability.can?(:destroy, object) && additional_condition if condition.nil?
+
+
+    unless condition
+      append_to_flash(:error, error_message || "#{name.upcase_first} could not be destroyed.")
+      append_to_flash(:error, "You do not have permission to destroy #{name}.") if ability.cannot?(:destroy, object)
+      append_to_flash(:error, "The additional condition to destroy #{name} was not fulfilled.") unless additional_condition || additional_condition.nil?
+      
+      return false
+    end
 
     if object.destroy
-      append_to_flash(:success, success_message || "#{name} has been successfully destroyed.")
+      append_to_flash(:success, success_message || "#{name.upcase_first} has been successfully destroyed.")
 
       return true
     else
-      append_to_flash(:error, error_message || "#{name} could not be destroyed.")
+      append_to_flash(:error, error_message || "#{name.upcase_first} could not be destroyed.")
       if append_errors_to_error_flash
         object.errors.messages[:destroy].each do |message|
           append_to_flash(:error, message)
