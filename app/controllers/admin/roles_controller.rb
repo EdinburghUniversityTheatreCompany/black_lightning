9826d8ac6fd02da98ef96afc103881961e99ec13
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-24 16:06:41 +0200

    Use Generic Controller for Admin Roles controller Fix bug where one of the roles controller tests occassionally fails because someone's name contains an '

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -1,61 +1,19 @@
 class Admin::RolesController < AdminController
+  include GenericController
+
   load_and_authorize_resource
-
-  def index
-    @title = 'Roles'
-  end
-
+  
   def show
-    @title = @role.name
-
     @q = @role.users.ransack(params[:q])
 
     @users = @q.result
                .accessible_by(current_ability)
 
-    # Using a token is not the nicest way of handling it, but it works.
+    # Using a token is not the nicest way of handling the adding, but it works.
+    # It's a bit annoying because /shared/form/user_field needs a model to work.
     @token = MembershipActivationToken.new
-  end
 
-  def new
-    # The title is set by the view.
-  end
-
-  def create
-    respond_to do |format|
-      if @role.save
-        format.html { redirect_to [:admin, @role], notice: 'Role was successfully created.' }
-        format.json { render json: [:admin, @role], status: :created, location: @role }
-      else
-        format.html { render 'new', status: :unprocessable_entity }
-        format.json { render json: @role.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def edit
-    # The title is set by the view.
-  end
-
-  def update
-    respond_to do |format|
-      if @role.update(role_params)
-        format.html { redirect_to admin_role_url(@role), notice: 'Role was successfully updated.' }
-        format.json { render json: [:admin, @role], status: :updated, location: @role }
-      else
-        format.html { render 'edit', status: :unprocessable_entity }
-        format.json { render json: @role.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  def destroy
-    helpers.destroy_with_flash_message(@role)
-
-    respond_to do |format|
-      format.html { redirect_to admin_roles_path }
-      format.json { format.json { head :no_content } }
-    end
+    super
   end
 
   def add_user
@@ -80,7 +38,11 @@ class Admin::RolesController < AdminController
 
   private
 
-  def role_params
-    params.require(:role).permit(:name)
+  def permitted_params
+    [:name]
+  end
+
+  def order_args
+    :name
   end
 end
