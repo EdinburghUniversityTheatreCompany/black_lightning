e8b4cbc07d10efc84f2c783bfcd106bcd0199744
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-24 16:01:51 +0200

    Use Generic Controller for Venues controller

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -2,21 +2,13 @@
 # Public controller for Venue. More details can be found there.
 ##
 class VenuesController < ApplicationController
-  load_and_authorize_resource
-  ##
-  # GET /venues
-  #
-  # GET /venues.json
-  ##
-  def index
-    @title = 'Venues'
-    @venues = @venues.includes(image_attachment: :blob).paginate(page: params[:page], per_page: 20)
+  include GenericController
 
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @venues }
-    end
-  end
+  load_and_authorize_resource
+
+  ##
+  # Has include_args
+  ##
 
   ##
   # GET /venues/1
@@ -24,13 +16,14 @@ class VenuesController < ApplicationController
   # GET /venues/1.json
   ##
   def show
-    @title = @venue.name
-
     @current_shows = @venue.shows.current.first(3)
 
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @venue }
-    end
+    super
+  end
+
+  private 
+  
+  def includes_args
+    [image_attachment: :blob]
   end
 end
