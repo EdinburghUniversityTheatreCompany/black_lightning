0de0c7dbdc2a1c1d7345b4872116c017b8013d40
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-24 16:11:47 +0200

    Use Generic Controller for Admin Debt Notifications controller Remove unnecessary loading of the search_params into environment variables

--- app/controllers/admin/debt_notifications_controller.rb
+++ app/controllers/admin/debt_notifications_controller.rb
@@ -1,24 +1,18 @@
 class Admin::DebtNotificationsController < AdminController
-  skip_load_and_authorize_resource
-  # GET /admin/debt_notifications
-  def index
-    authorize! :index, Admin::DebtNotification
-    @title = 'Debt Notifications'
+  include GenericController
 
-    get_search_params(params)
+  load_and_authorize_resource
+
+  private
+
+  def base_index_query
     @q = Admin::DebtNotification.ransack(params[:q])
     @q.sorts = ['sent_on desc', 'user_full_name asc'] if @q.sorts.empty?
 
-    @debt_notifications = @q.result(distinct: true).includes(:user).paginate(page: params[:page], per_page: 15)
-    respond_to do |format|
-      format.html
-      format.json { render json: @debt_notifications }
-    end
+    return @q.result(distinct: true).includes(:user)
   end
 
-  def get_search_params(params)
-    q = params[:q] || {}
-    @first_name = q.fetch(:user_first_name_cont, '')
-    @last_name = q.fetch(:user_last_name_cont, '')
+  def resource_class
+    Admin::DebtNotification
   end
 end
