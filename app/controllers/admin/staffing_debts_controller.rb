09f2f2dd0049562a81fcc89bcaf54edc0c830bf7
Mick Zijdel <mickzijdel@hotmail.com>
2020-06-24 16:25:19 +0200

    Use Generic Controller for Admin Staffing Debts controller

--- app/controllers/admin/staffing_debts_controller.rb
+++ app/controllers/admin/staffing_debts_controller.rb
@@ -1,67 +1,23 @@
 # These views are very similar to those for maintenance_debts, so if you have improvements for this, have a look if you can apply them there as well.
 class Admin::StaffingDebtsController < AdminController
+  include GenericController
+
   load_and_authorize_resource
 
-  # GET /admin/staffing_debts
-  def index
-    @title = 'Staffing Debts'
-
-    @staffing_debts, @q, @show_fulfilled, @is_specific_user = helpers.shared_debt_load(@staffing_debts, params, [:user, :admin_staffing_job])
-
-    respond_to do |format|
-      format.html
-      format.json { render json: @staffing_debts }
-    end
-  end
+  ##
+  # Overrides load_index_resources
+  ##
 
   # GET /admin/staffing_debts/1
   def show
     @title = "Staffing Debt for #{@staffing_debt.user.name(current_user)}"
 
     @jobs = @staffing_debt.user.staffing_jobs.unassociated_staffing_jobs_that_count_towards_debt
+
+    super
   end
 
-  # GET /admin/staffing_debts/new
-  def new
-    respond_to do |format|
-      format.html
-      format.json { render json: @staffing_debt }
-    end
-  end
-
-  # POST /admin/staffing_debts
-  def create
-    respond_to do |format|
-      if @staffing_debt.save
-        format.html { redirect_to @staffing_debt, notice: 'The Staffing Debt was successfully created.' }
-        format.json { head :no_content }
-      else
-        format.html { render 'new', status: :unprocessable_entity, notice: 'Failed to create new Staffing Debt. Please try again or contact IT' }
-        format.json { render json: @staffing_debt.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # GET /admin/staffing_debts/1/edit
-  def edit
-    respond_to do |format|
-      format.html
-      format.json { render json: @staffing_debt }
-    end
-  end
-
-  # PATCH/PUT /admin/staffing_debts/1
-  def update
-    respond_to do |format|
-      if @staffing_debt.update(staffing_debt_params)
-        format.html { redirect_to @staffing_debt, notice: 'The Staffing Debt was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render 'edit', status: :unprocessable_entity, notice: 'Failed to update the Staffing Debt. Please try again or contact IT.' }
-        format.json { render json: @staffing_debt.errors, status: :unprocessable_entity }
-      end
-    end
-  end
+  # New, Create, Edit, and Update are handled by the Generic Controller.
 
   # DELETE /admin/staffing_debts/1
   def destroy
@@ -117,8 +73,17 @@ class Admin::StaffingDebtsController < AdminController
 
   private
 
-  # Only allow a trusted parameter "white list" through.
-  def staffing_debt_params
-    params.require(:admin_staffing_debt).permit(:user_id, :show_id, :due_by, :admin_staffing_job_id)
+  def resource_class
+    Admin::StaffingDebt
+  end
+
+  def permitted_params
+    [:user_id, :show_id, :due_by, :admin_staffing_job_id]
+  end
+
+  def load_index_resources
+    @staffing_debts, @q, @show_fulfilled, @is_specific_user = helpers.shared_debt_load(@staffing_debts, params, [:user, :admin_staffing_job])
+
+    return @staffing_debts
   end
 end
