2356612671f3455f5f9b01a34f8af961d0f831d3
Mick Zijdel <mickzijdel@hotmail.com>
2020-08-13 19:10:29 +0200

    Add the new admin/resources controller, for real this time

--- app/controllers/admin/resources_controller.rb
+++ app/controllers/admin/resources_controller.rb
@@ -1,29 +1,18 @@
 class Admin::ResourcesController < AdminController
   layout 'subpage_sidebar'
 
-  def index
-    set_subpages('')
-    render 'admin/resources/overview'
-  end
-
   def page
-    if params[:page].nil? || params[:page] == '' || params[:page].downcase == 'overview'
-      index
-    else
-      set_subpages(params[:page])
-      begin
-        render 'admin/resources/' + params[:page]
-      rescue ActionView::MissingTemplate
-        redirect_to '404', status: 404
-      end
-    end
-  end
-
-  private
-
-  def set_subpages(page)
     @controller = 'admin/resources'
-    @root_page = helpers.get_subpage_root_page(page)
-    @subpages = helpers.get_subpages(@controller, @root_page)
+
+    root_url = helpers.get_subpage_root_url(@controller, params[:page])
+
+    @editable_block = Admin::EditableBlock.find_by(url: root_url)
+
+    if @editable_block.nil?
+      redirect_to '404', status: 404
+      return
+    end
+
+    @subpages = helpers.get_subpages(root_url)
   end
 end
