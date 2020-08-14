0d8448013599252781fc616732d4affb8d3d481d
Mick Zijdel <mickzijdel@hotmail.com>
2020-08-14 09:51:00 +0200

    Add seed with a few default Editable Blocks for the Subpage pages

--- db/seeds.rb
+++ db/seeds.rb
@@ -14,3 +14,13 @@ admin_user.add_role :admin
 
 test_user = User.create! email: 'test@bedlamtheatre.co.uk', password: 'Passw0rd', password_confirmation: 'Passw0rd'
 test_user.add_role :member
+
+# Necessary Editable Blocks
+
+about = Admin::EditableBlock.create!              url: 'about', name: 'About', group: 'About'
+
+get_invovled = Admin::EditableBlock.create!       url: 'get_involved',                name: 'Get Involved',   group: 'Get Involved'
+about = Admin::EditableBlock.create!              url: 'get_involved/opportunities',  name: 'Opportunities',  group: 'Get Involved'
+
+resources = Admin::EditableBlock.create!          url: 'admin/resources',                     name: 'Resources',          group: 'Resources'
+membership_checker = Admin::EditableBlock.create! url: 'admin/resources/membership_checker',  name: 'Membership Checker', group: 'Resources'
