151a9e7003481d397da9524b2da3fad0c4b992b1
Mick Zijdel <mickzijdel@hotmail.com>
2020-12-07 21:13:55 +0000

    Update capistrano to work with Webpacker

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,14 +8,15 @@ set :user, 'deploy'
 set :keep_releases, 4
 
 set :linked_files, %w(config/database.yml config/secrets.yml config/openid_signing_key)
-set :linked_dirs, %w(log bundle tmp/pids tmp/cache tmp/sockets public/system public/assets uploads storage)
+set :linked_dirs, %w(log bundle tmp/pids tmp/cache tmp/sockets public/system public/assets public/packs node_modules uploads storage)
 
 # TODO: Run zeitwerk:check
 # TODO: Run tests
 # TODO: Fix the issue with delayed_job not restarting properly
 # TODO: Do a mysql dump
 
-# If you are using Passenger mod_rails uncomment this:
+before "deploy:assets:precompile", "deploy:yarn_install"
+
 namespace :deploy do
   after :publishing, :restart do
     on roles(:app), in: :sequence, wait: 5 do
@@ -31,6 +32,15 @@ namespace :deploy do
       within release_path do
         execute :echo, "#{capture("cd #{repo_path} && git describe --always --tags")} > version"
       end
+    end
+  end
+
+  # Taken from https://github.com/rails/webpacker/blob/master/docs/deployment.md
+  desc "Run rake yarn install"
+  task :yarn_install do
+    on roles(:web) do
+      within release_path do
+        execute("source ~/.nvm/nvm.sh && cd #{release_path} && yarn install --silent --no-progress --no-audit --no-optional")      end
     end
   end
 end
