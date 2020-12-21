602df7ecf73b32c3c6bd3c5b4822b59e18c6a385
Mick Zijdel <mickzijdel@hotmail.com>
2020-12-21 21:34:00 +0100

    Fix the proposals archive, again. Hopefully, for real this time

--- app/controllers/archives/proposals_controller.rb
+++ app/controllers/archives/proposals_controller.rb
@@ -13,12 +13,16 @@ class Archives::ProposalsController < AdminController
     result_ids = @proposals.ids
 
     @proposals = Admin::Proposals::Proposal.where(id: result_ids).accessible_by(current_ability)
-    call_ids = @proposals.collect(&:call_id)
+    call_ids = @proposals.collect(&:call_id).uniq
 
     @calls = Admin::Proposals::Call.where(id: call_ids)
-                                   .unscoped.order('submission_deadline ASC')
+                                   .unscoped
+                                   .order('submission_deadline DESC')
                                    .paginate(page: params[:page], per_page: 20)
 
-    @proposals = @proposals.where(call_id: call_ids).includes(:call, team_members: [user: [:admin_maintenance_debts, :admin_staffing_debts]]).group_by(&:call)
+    @proposals = @proposals.where(call_id: @calls.ids)
+                           .includes(:call, team_members: [user: [:admin_maintenance_debts, :admin_staffing_debts]])
+                           .order('admin_proposals_calls.submission_deadline DESC')
+                           .group_by(&:call)
   end
 end
