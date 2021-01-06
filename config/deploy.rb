eff5558bb7d54186c0cc253b3cd10a83753302d9
Mick Zijdel <mickzijdel@hotmail.com>
2021-01-06 12:56:02 +0100

    Add .duplicacy to the linked dirs for capistrano so it is kept when deploying

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,7 +8,7 @@ set :user, 'deploy'
 set :keep_releases, 4
 
 set :linked_files, %w(config/database.yml config/secrets.yml config/openid_signing_key)
-set :linked_dirs, %w(log bundle tmp/pids tmp/cache tmp/sockets public/system public/assets public/packs node_modules uploads storage)
+set :linked_dirs, %w(log bundle tmp/pids tmp/cache tmp/sockets public/system public/assets public/packs node_modules uploads storage .duplicacy)
 
 # TODO: Run zeitwerk:check
 # TODO: Run tests
