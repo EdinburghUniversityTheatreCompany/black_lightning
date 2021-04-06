31aba8567f20e91e866d66d1d1b3d43de48db01c
Mick Zijdel <mickzijdel@hotmail.com>
2021-04-06 21:15:29 +0200

    Add bullet and do some N+1 query optimisations

--- app/models/concerns/attachment_item.rb
+++ app/models/concerns/attachment_item.rb
@@ -2,7 +2,8 @@ module AttachmentItem
   extend ActiveSupport::Concern
 
   included do
-    has_many :attachments, class_name: '::Attachment', as: :item
+    has_many :attachments, -> { includes(:attachment_tags, :attachment_tags_attachments, { file_attachment: :blob }) },
+             class_name: '::Attachment', as: :item
     accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
   end
 end
