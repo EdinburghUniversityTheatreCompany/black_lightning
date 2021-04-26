f6a732d856f195bb22b1300461cbd36aba8a773b
Mick Zijdel <mickzijdel@hotmail.com>
2021-04-26 12:19:41 +0200

    Also notify the president of a Complaint

--- app/mailers/complaints_mailer.rb
+++ app/mailers/complaints_mailer.rb
@@ -6,7 +6,7 @@ class ComplaintsMailer < ActionMailer::Base
 
     @creation_time_string = l(@complaint.created_at, format: :longy)
 
-    receiver = 'welfare@bedlamtheatre.co.uk'
+    receiver = ['welfare@bedlamtheatre.co.uk', 'president@bedlamtheatre.co.uk']
 
     mail(to: receiver, subject: "New Complaint Submitted on #{@creation_time_string}")
   end
