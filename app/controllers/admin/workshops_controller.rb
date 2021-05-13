00c97c47b30ebc61f0c2236549628160534e6dd9
Mick Zijdel <mickzijdel@hotmail.com>
2021-05-13 16:58:12 +0200

    Retitle workshops controller to workshops and events controller in the admin bit

--- app/controllers/admin/workshops_controller.rb
+++ app/controllers/admin/workshops_controller.rb
@@ -2,6 +2,8 @@ class Admin::WorkshopsController < Admin::EventsController
   # GET /admin/shows
   # GET /admin/shows.json
   def index
+    @title = 'Workshops and Events'
+    
     @editable_block_name = 'Workshops (Members Face)'
     @url = :admin_workshops
 
