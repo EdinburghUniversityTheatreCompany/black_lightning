a431b4743335a51dd0e1cca5f92c424385f76a6c
Mick Zijdel <mickzijdel@hotmail.com>
2021-08-04 22:30:44 +0200

    Add preview for debts mailer Fine-tune mail_debtor email

--- app/mailers/debt_mailer.rb
+++ app/mailers/debt_mailer.rb
@@ -1,11 +1,15 @@
 # Not directly tested, but the debt task is, so it is covered.
 class DebtMailer < ApplicationMailer
+  default reply_to: 'FOH and Theatre Manager <debt@bedlamtheatre.co.uk>'
+
   def mail_debtor(user, new_debtor)
     @user = user
     @new_debtor = new_debtor
 
     @subject = new_debtor ? 'Notification of Debt' : 'Reminder of Debt'
     notification_type = new_debtor ? :initial_notification : :reminder
+
+    @debt_moment = Admin::Debt.users_oldest_debt(@user.id) || '|||ERROR: No moment of debt found. Please email it@bedlamtheatre.co.uk|||'
 
     Admin::DebtNotification.create(user: @user, sent_on: Date.today, notification_type: notification_type)
     return mail(to: @user.email, subject: @subject)
---
+++ test/mailers/previews/debt_mailer_preview.rb
@@ -0,0 +1,8 @@
+class DebtMailerPreview < ActionMailer::Preview
+  def mail_debtor
+    user = User.all.sample
+    new_debtor = [true, false].sample
+
+    DebtMailer.mail_debtor(user, new_debtor)
+  end
+end
