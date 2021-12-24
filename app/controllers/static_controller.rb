0fe561d7412929e5c2c6c2004bc385c0bf8f9c49
Mick Zijdel <mickzijdel@hotmail.com>
2021-12-24 20:32:22 +0100

    Reorder events on the home page by start data so the earliest one is listed first, rather than the one furthest away.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -14,7 +14,7 @@ class StaticController < ApplicationController
 
   def home
     @news = News.accessible_by(current_ability).includes(image_attachment: :blob).order('publish_date DESC').first(2)
-    @events = Event.includes(image_attachment: :blob).current.order('start_date ASC')
+    @events = Event.includes(image_attachment: :blob).current.reorder('start_date ASC')
 
     @last_event = Event.last_event
   end
