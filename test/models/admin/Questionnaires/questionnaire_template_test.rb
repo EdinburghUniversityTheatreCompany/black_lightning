ef3e64671d08cf2699e0a6dfaac94781e03cdc20
Mick Zijdel <mickzijdel@hotmail.com>
2021-12-25 10:27:44 +0100

    Run annotate

--- test/models/admin/Questionnaires/questionnaire_template_test.rb
+++ test/models/admin/Questionnaires/questionnaire_template_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: admin_questionnaires_questionnaire_templates
+#
+# *id*::         <tt>integer, not null, primary key</tt>
+# *name*::       <tt>string(255)</tt>
+# *created_at*:: <tt>datetime, not null</tt>
+# *updated_at*:: <tt>datetime, not null</tt>
+#--
+# == Schema Information End
+#++
 require 'test_helper'
 
 class Admin::Questionnaires::QuestionnaireTemplateTest < ActiveSupport::TestCase
