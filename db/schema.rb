2597d3b7fd7da5d1198fe2a606a764c1b8307996
Alexander Mohan Morzeria-Davis <me+git@alexandermohan.com>
2022-07-04 19:43:30 +0200

    Initial upgrade to admin-lte, currently broken
    Signed-off-by: Alexander Mohan Morzeria-Davis <me+git@alexandermohan.com>

--- db/schema.rb
+++ db/schema.rb
@@ -559,8 +559,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_130421) do
     t.string "username"
     t.string "remember_token"
     t.date "consented"
-    t.string "activation_state", default: "neutral", null: false
-    t.boolean "ever_activated", default: false, null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
@@ -606,7 +604,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_19_130421) do
     t.index ["item_type", "item_id"], name: "index_video_links_on_item_type_and_item_id"
   end
 
-  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  create_table "vouchers", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "voucher"
+    t.boolean "is_active"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "admin_debt_notifications", "users"
   add_foreign_key "events", "admin_proposals_proposals", column: "proposal_id"
