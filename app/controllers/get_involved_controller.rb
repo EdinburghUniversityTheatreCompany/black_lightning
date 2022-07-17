97477121c988a33bf58822d6c31db8f0c03a8b78
Mick Zijdel <mickzijdel@hotmail.com>
2022-07-17 07:25:46 +0100

    Remove subpage sidebar things from the get_involved and about sections on the public front-end

--- app/controllers/get_involved_controller.rb
+++ app/controllers/get_involved_controller.rb
@@ -6,29 +6,13 @@
 class GetInvolvedController < ApplicationController
   skip_authorization_check
 
-  layout 'subpage_sidebar'
-
   def opportunities
     @opportunities = Opportunity.active
-
-    set_subpages
 
     @editable_block = Admin::EditableBlock.find_by(url: 'get_involved/opportunities')
   end
 
   def page
-    set_subpages
-
-    @editable_block = Admin::EditableBlock.find_by!(url: @root_url)
-  end
-
-  private
-
-  def set_subpages
-    @controller = 'get_involved'
-
-    @root_url = helpers.get_subpage_root_url(@controller, params[:page])
-
-    @subpages = helpers.get_subpages(@root_url)
+    @editable_block = Admin::EditableBlock.find_by!(url: @current_path.delete_prefix('/'))
   end
 end
