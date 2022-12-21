1ac95a139ac8eeb9517ac79f682acbab077447a6
Mick Zijdel <mickzijdel@hotmail.com>
2022-12-21 22:03:44 +0100

    Fix select2 width

--- app/inputs/collection_select_input.rb
+++ app/inputs/collection_select_input.rb
@@ -13,6 +13,11 @@ class CollectionSelectInput < SimpleForm::Inputs::CollectionSelectInput
       merged_input_options = merged_input_options.merge({ class: 'simple-select2' })
     end
 
+    # Set the width to 100% for select2 fields if they have no width set so they do not shrink unreasonably.
+    # This means inline fields need to have their width specified or be columned.
+    style = merged_input_options[:style]
+    merged_input_options[:style] =  "width: 100%; #{style}" if style.present? && !style.include?('width:')
+
     @builder.collection_select(
       attribute_name, collection, value_method, label_method,
       input_options, merged_input_options
