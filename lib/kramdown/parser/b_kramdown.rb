38b4ca3f65495a9a5ae6e92dd67eede23cf47b96
Mick Zijdel <mickzijdel@hotmail.com>
2022-12-22 18:00:34 +0100

    Add and remove some todo's

--- lib/kramdown/parser/b_kramdown.rb
+++ lib/kramdown/parser/b_kramdown.rb
@@ -4,7 +4,8 @@ class Kramdown::Parser::BKramdown < Kramdown::Parser::Kramdown
     @options = options
   end
 
-  # BOOTSTRAP NICETOHAVE: Kramdown images also need to reserve space to prevent content reflow. Not sure where to start
+  # BOOTSTRAP NICETOHAVE: Kramdown images also need to reserve space to prevent content reflow. Not sure where to start.
+  # There might be something in the shared_additions file.
 
   def handle_extension(name, opts, body, type, _line_no)
     case name
