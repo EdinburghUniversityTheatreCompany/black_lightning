5d2c5dd82cddf596e74599e452b3de706111f1e1
Alexander Mohan Morzeria-Davis <me+git@alexandermohan.com>
2022-12-26 16:49:27 +0000

    fix: not working select2-with-tags

--- app/inputs/collection_select_input.rb
+++ app/inputs/collection_select_input.rb
@@ -3,14 +3,14 @@
 
 class CollectionSelectInput < SimpleForm::Inputs::CollectionSelectInput
   def input(wrapper_options = nil)
-    label_method, value_method = detect_collection_methods
+    label_method, value_method =                                                                  detect_collection_methods
 
     merged_input_options = merge_wrapper_options(input_html_options, wrapper_options)
 
     merged_input_options = merged_input_options.merge({ class: 'simple-select2' })
 
     # If it allows custom input, set the 'select2-with-tags' attribute to true so it will be recognised by the script that instantiates the select2 fields.
-    merged_input_options['select2-with-tags'] = 'true' if input_options.try(:allow_custom_input)
+    merged_input_options['select2-with-tags'] = 'true' if input_options[:allow_custom_input]
 
     @builder.collection_select(
       attribute_name, collection, value_method, label_method,
