b52ce01b14f6b808d71b2526df53154f0a6dd4cf
Mick Zijdel <mickzijdel@hotmail.com>
2022-12-30 10:01:15 +0000

    Fix escape_line_breaks formatting_helper method to not break when passed a nil value

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -8,6 +8,6 @@ module FormattingHelper
   end
 
   def escape_line_breaks(content)
-    return content.gsub(/[\r\n]+/, "<br />").html_safe 
+    return content&.gsub(/[\r\n]+/, "<br />")&.html_safe 
   end
 end
