c1f97955c61b554b0f738d592ee6258915bec39f
Mick Zijdel <mickzijdel@hotmail.com>
2023-06-23 15:34:23 +0100

    Add controller and views to view, create and edit reviews directly

---
+++ app/controllers/admin/reviews_controller.rb
@@ -0,0 +1,18 @@
+##
+# Admin controller for Venue management.
+##
+class Admin::ReviewsController < AdminController
+  include GenericController
+
+  load_and_authorize_resource
+
+  private
+
+  def permitted_params
+    [:title, :url, :body, :rating, :review_date, :organisation, :reviewer, :event_id]
+  end
+
+  def order_args
+    ['review_date DESC']
+  end
+end
