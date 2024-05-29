15097ab7cc785056917dd3a3da0b51effa5898b6
Mick Zijdel <mickzijdel@hotmail.com>
2024-05-28 21:08:37 -0400

    Remove unused format helper and test an escape_line_breaks method

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -1,12 +1,4 @@
 module FormattingHelper
-  def format(content, tag)
-    if content.is_a? Array
-      return content.map { |item| "<#{tag}>#{item}</#{tag}>".html_safe }
-    else
-      return "<#{tag}>#{item}</#{tag}>".html_safe
-    end
-  end
-
   def escape_line_breaks(content)
     return content&.gsub(/[\r\n]+/, "<br />")&.html_safe 
   end
