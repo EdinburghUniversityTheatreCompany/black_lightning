329267a72f59503e1b920439e67b4976cd1c2eeb
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-13 09:10:45 +0100

    Improve error rendering in alert_div and alert_message so that all flash messages are displayed, rather than just one.
    It makes a list out of messages of the same type, and puts it in the same alert modal, and consecutively displays them if there are errors of multiple types.

--- app/helpers/formatting_helper.rb
+++ app/helpers/formatting_helper.rb
@@ -2,4 +2,9 @@ module FormattingHelper
   def escape_line_breaks(content)
     return content&.gsub(/[\r\n]+/, "<br />")&.html_safe 
   end
+
+  def render_as_list(list, wrap_tag)
+    list.map! { |item| ActionController::Base.helpers.sanitize(item) }
+    "<#{wrap_tag}><li>#{list.join('</li><li>')}</li></#{wrap_tag}>".html_safe
+  end
 end
