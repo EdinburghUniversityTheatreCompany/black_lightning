3d16935409f14ea0c2dda32f9cd5394acf7d5460
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-16 08:29:49 +0100

    Upgrade packages to Rails 7.1

--- babel.config.js
+++ babel.config.js
@@ -42,18 +42,6 @@ module.exports = function(api) {
       isTestEnv && 'babel-plugin-dynamic-import-node',
       '@babel/plugin-transform-destructuring',
       [
-        '@babel/plugin-proposal-class-properties',
-        {
-          loose: true
-        }
-      ],
-      [
-        '@babel/plugin-proposal-object-rest-spread',
-        {
-          useBuiltIns: true
-        }
-      ],
-      [
         '@babel/plugin-transform-private-methods',
         {
           loose: true
