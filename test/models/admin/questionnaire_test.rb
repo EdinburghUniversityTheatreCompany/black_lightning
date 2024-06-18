72ce89bb0e85c2e5fcddba5f20891defa4e44ebe
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-18 12:48:52 +0100

    Fix questionnaire test for instantiating answers

--- test/models/admin/questionnaire_test.rb
+++ test/models/admin/questionnaire_test.rb
@@ -4,11 +4,13 @@ class Admin::QuestionnaireTest < ActiveSupport::TestCase
   test 'instantiate answers' do
     questionnaire = FactoryBot.create(:questionnaire)
 
-    unanswered_questions = questionnaire.questions.to_a.count { |question| question.answers.where(answerable: questionnaire).empty? }
+    # Remove answers from every question so they are all unanswered.
+    questionnaire.questions.each { |question| question.answers.delete_all }
 
-    assert_not_equal 0, unanswered_questions, 'All questionnaire questions are already answered, so this test does not test anything. Change this test so not all questions are answered.'
+    assert_not_equal 0, questionnaire.questions.count, 'All questionnaire questions are already answered, so this test does not test anything. Change this test so not all questions are answered.'
 
-    assert_difference 'questionnaire.answers.count', unanswered_questions do
+    # Need to create as many answers as there are questions, as they are all unanswered.
+    assert_difference 'questionnaire.answers.count', questionnaire.questions.count do
       questionnaire.instantiate_answers!
     end
   end
