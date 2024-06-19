ecf24c4c75e4195b79376c71a87c7ae6d7109f2e
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-19 19:19:46 +0100

    Fix controllers and dropzone imports

--- app/javascript/shared.js
+++ app/javascript/shared.js
@@ -5,9 +5,11 @@ import 'jquery-slimscroll'
 import './src/shared/konami_code'
 import './src/shared/md_editor'
 
-//import "./controllers"
+// Load all the stimulus controllers
+import "./controllers"
 
 require("@rails/activestorage").start()
 
 // Requires jQuery. There are vanilla js packages, but not as frequently maintained or downloaded.
 require("@nathanvda/cocoon")
+
