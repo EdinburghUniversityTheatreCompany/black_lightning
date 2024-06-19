aa229b16ee1dea4fe94688ddd4c98803d4bb1dea
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-19 20:56:00 +0100

    Rewrite the staffing_form_dynamic_date_fields form to vanilla JavaScript

--- app/assets/javascripts/admin/staffing_form_dynamic_date_fields.js
+++ app/assets/javascripts/admin/staffing_form_dynamic_date_fields.js
@@ -1,24 +1,31 @@
-$(function () {
+document.addEventListener('DOMContentLoaded', function() {
     var counter = 0;
-
-    $('#add_date').click(function () {
-        // Clone the blueprint, show it, and remove the blueprint id.
-        var new_date_fields = $('#date_blueprint').clone();
-        $(new_date_fields).show();
-        $(new_date_fields).removeAttr('id');
-
-        // Rename the newly-added item so the params are formatted correctly.
-        new_date_fields.find('#start_datetime').attr('name', 'start_times[' + counter + ']');
-        new_date_fields.find('#end_time').attr('name', 'end_times[' + counter + ']');
-
-        // Remove the current row if clicked.
-        new_date_fields.find('a').click(function () {
-            $(new_date_fields).remove();
-        });
-        // Add one to the counter for the ids
-        counter++;
-
-        // Add the newly-generated item right before the add_date button.
-        $('#add_date').before(new_date_fields);
+    var addDateButton = document.getElementById('add_date');
+    var dateBlueprint = document.getElementById('date_blueprint');
+  
+    addDateButton.addEventListener('click', function() {
+      // Clone the blueprint, show it, and remove the blueprint id.
+      var newDateFields = dateBlueprint.cloneNode(true);
+      newDateFields.style.display = 'block';
+      newDateFields.removeAttribute('id');
+  
+      // Rename the newly-added item so the parameters are formatted correctly.
+      var startDatetime = newDateFields.querySelector('#start_datetime');
+      startDatetime.setAttribute('name', 'start_times[' + counter + ']');
+  
+      var endTime = newDateFields.querySelector('#end_time');
+      endTime.setAttribute('name', 'end_times[' + counter + ']');
+  
+      // Remove the current row if clicked.
+      var removeButton = newDateFields.querySelector('a');
+      removeButton.addEventListener('click', function() {
+        newDateFields.remove();
+      });
+  
+      // Add one to the counter for the ids
+      counter++;
+  
+      // Add the newly-generated item right before the add_date button.
+      addDateButton.parentNode.insertBefore(newDateFields, addDateButton);
     });
-});
+  });
