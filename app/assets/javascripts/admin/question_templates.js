6071bcc67b887f208d6b7ec837ca03d07e6051ea
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-19 23:30:34 +0100

    Reformat question_templates

--- app/assets/javascripts/admin/question_templates.js
+++ app/assets/javascripts/admin/question_templates.js
@@ -1,140 +1,142 @@
-// I feel like having this global is not the neatest way of doing it, but I cannot think of another way.
-// This stores the current item from the template that should be added.
-var template_item = null;
-// The type of the items. Either questions or jobs.
-var items_type = null;
-// Store all data for the question_template or staffing_jobs template to be loaded.
-var global_data = null;
+// Execute the script after the DOM is fully loaded
+document.addEventListener('DOMContentLoaded', () => {
+  new TemplateLoader();
+});
 
-(function () {
-  "use strict";
+// Constants for item types to avoid typos and improve readability
+const ITEMS_TYPE = {
+  QUESTIONS: 'questions',
+  JOBS: 'jobs'
+};
 
-  // You need to set a meta tag with the templates base url and the items_type.
-  // Do not end the url with a slash.
-  // See questionnaires form for an example on how to do this.
-  // Make sure you set the meta tag ABOVE the javascript include tag
-  var templates_base_url = $('meta[name="templates-base-url"]').attr("content");
-  items_type = $('meta[name="templates-items-type"]').attr("content");
+class TemplateLoader {
+  constructor() {
+    // Get base URL and items type from meta tags in the HTML
+    // You need to set a meta tag with the templates base url and the items_type.
+    // Do not end the url with a slash.
+    // See questionnaires form for an example on how to do this.
+    // Make sure you set the meta tag ABOVE the javascript include tag  
+    this.templatesBaseUrl = $('meta[name="templates-base-url"]').attr("content");
+    this.itemsType = $('meta[name="templates-items-type"]').attr("content");
+    this.templateItem = null; // Holds the current item being added from the template
+    this.globalData = null;  // Stores all data for the selected template
 
-  if(templates_base_url == null) {alert("'templates_base_url' is null. Is it set properly?")};
-  if(items_type == null || (items_type != 'questions' && items_type != 'jobs')) {alert("'items_type' is null or not 'questions' or 'jobs'. Is it set properly?")};
+    this.validateSetup();
+    this.initEventListeners();
+  }
 
-  $(document)
-    // This is the bit where the actual data is inserted.
-    .on('cocoon:after-insert', function(e, insertedItem, originalEvent) {
-      if (template_item === null) { return; }
-  
-      // If the type is questions, the added item can either be a question or a notify email.
-      if(items_type == 'questions')
-      {
-        if(insertedItem.hasClass('question'))
-        {
-          insertedItem.find('[name$="[question_text]"]').val(template_item.question_text);
-          insertedItem.find('[name$="[response_type]"]').val(template_item.response_type);
-        }
-        else if (insertedItem.hasClass('email'))
-        {
-          insertedItem.find('[name$="[email]"]').val(template_item.email)
-        }
-      }
-      else if(items_type == 'jobs')
-      {
-        insertedItem.find('[name$="[name]"]').val(template_item.name);
-      }
-    })
-
-  // Start inserting the fields
-  function loadTemplate() {
-    console.log('loadTemplate')
-    if(items_type == 'questions')
-    {
-      loadTemplateHelper('question_add_button', global_data.questions);
-      loadTemplateHelper('notify_email_add_button', global_data.notify_emails);
+  // Validate that required meta tags are set correctly
+  validateSetup() {
+    if (!this.templatesBaseUrl) {
+      alert("'templates_base_url' is null. Is it set properly?");
     }
-    else if(items_type == 'jobs')
-    {
-      loadTemplateHelper('staffing_job_add_button', global_data.staffing_jobs);
+    if (!this.itemsType || ![ITEMS_TYPE.QUESTIONS, ITEMS_TYPE.JOBS].includes(this.itemsType)) {
+      alert("'items_type' is null or not 'questions' or 'jobs'. Is it set properly?");
     }
   }
 
-  function loadTemplateHelper(add_fields_button_class, template_items)
-  {
-    // TODO: This feels dirty - there must be a better way.
-    // This loops over every item in the template and clicks the corresponding 'add fields' button.
-    // This then triggers the cocoon:after-insert event (see above) which will insert the data into the fields.
-    $.each(template_items, function (index, item) {
-      template_item = item;
-      console.log('triggering click on ' + add_fields_button_class);
-      console.log($('.' + add_fields_button_class).first());
-      $('.' + add_fields_button_class).first().trigger('click');
-      console.log('after click')
+  // Initialize all event listeners
+  initEventListeners() {
+    // Listen for Cocoon's after-insert event to populate new fields
+    $(document).on('cocoon:after-insert', this.handleCocoonInsert.bind(this));
+
+    // Listen for changes in the template dropdown to load template data
+    $('#template_list').on('change', this.getTemplate.bind(this));
+
+    // Load the initial list of available templates
+    this.loadTemplateList();
+  }
+
+  // Handle Cocoon's after-insert event to populate the new fields with template data
+  handleCocoonInsert(e, insertedItem) {
+    if (!this.templateItem) return;
+
+    // If the type is questions, the added item can either be a question or a notify email.
+    if (this.itemsType === ITEMS_TYPE.QUESTIONS) {
+      if (insertedItem.hasClass('question')) {
+        // Populate question text and response type for questions
+        insertedItem.find('[name$="[question_text]"]').val(this.templateItem.question_text);
+        insertedItem.find('[name$="[response_type]"]').val(this.templateItem.response_type);
+      } else if (insertedItem.hasClass('email')) {
+      // Populate email for notify emails
+        insertedItem.find('[name$="[email]"]').val(this.templateItem.email);
+      }
+    } else if (this.itemsType === ITEMS_TYPE.JOBS) {
+      // Populate job name for staffing jobs
+      insertedItem.find('[name$="[name]"]').val(this.templateItem.name);
+    }
+    }
+
+  // Trigger the loading of template items when the "Load" button is clicked
+  loadTemplate() {
+    if (this.itemsType === ITEMS_TYPE.QUESTIONS) {
+        // For questions, load both questions and notify emails
+      this.loadTemplateHelper('question_add_button', this.globalData.questions);
+      this.loadTemplateHelper('notify_email_add_button', this.globalData.notify_emails);
+    } else if (this.itemsType === ITEMS_TYPE.JOBS) {
+      // For jobs, load staffing jobs
+      this.loadTemplateHelper('staffing_job_add_button', this.globalData.staffing_jobs);
+    }
+  }
+
+  // Helper method to load template items by triggering Cocoon's add button
+  loadTemplateHelper(addFieldsButtonClass, templateItems) {
+    templateItems.forEach(item => {
+      this.templateItem = item;
+  // This loops over every item in the template and clicks the corresponding 'add fields' button.
+      // This then triggers the cocoon:after-insert event (see above) which will insert the data into the fields.
+      $('.' + addFieldsButtonClass).first().trigger('click');
     });
 
     // After adding all the items, set the item back to null so adding a new field will add an empty field rather than the data from the last item in the template.
-    template_item = null;
+    this.templateItem = null;
   }
 
-  function getTemplate() {
-    var template_id = $('#template_list').val();
+  // Fetch the data for the template when a template is selected from the dropdown
+  getTemplate() {
+    const templateId = $('#template_list').val();
+    $('#template_load').off('click', this.loadTemplate.bind(this));
 
-    $('#template_load').off('click', loadTemplate);
-
-    // If selecting blank, reset the current data to null.
-    if (template_id === "") {
+    if (templateId === "") {
+      // If no template is selected, clear the summary and disable the load button
       $('#template_summary').empty();
-      global_data = null;
-
+      this.globalData = null;
       $('#template_load').addClass('disabled');
       return;
     }
-    $.getJSON(
-      templates_base_url + '/' + template_id + '.json',
-      function (data) {
-        global_data = data;
 
-        // Load the items that will be previewed in the modal when selecting a template.
-        var template_items = null;
-
-        if (items_type == 'questions')
-        {
-          template_items = data.questions.map(question => question.question_text);
-        }
-        else if(items_type == 'jobs')
-        {
-          template_items = data.staffing_jobs.map(job => job.name);
-        }
-
-        // Clear the old summary, readd the header, and add all the current preview items.
-        $('#template_summary').empty();
-
-        $('#template_summary').append('<h3>Items</h3>');
-
-        var items_list = $('<ul id="template_items_list"></ul>');
-
-        $.each(template_items, function (index, item_value) {
-          $(items_list).append('<li>' + item_value + '</li>');
-        });
-
-        $('#template_summary').append(items_list);
-
-        // Enable the load button after the data has been loaded.
-        $('#template_load').on('click', loadTemplate);
-        $('#template_load').removeClass('disabled');
-      }
-    );
+    // Fetch the template data from the server
+    $.getJSON(`${this.templatesBaseUrl}/${templateId}.json`, data => {
+      this.globalData = data;
+      this.updateTemplateSummary();
+      // Enable the load button and bind the loadTemplate method
+      $('#template_load').on('click', this.loadTemplate.bind(this));
+      $('#template_load').removeClass('disabled');
+    });
   }
 
-  // After loading the document, load the template list.
-  $(function () {
-    $.getJSON(
-      templates_base_url,
-      function (data) {
-        $.each(data, function (index, template) {
-          $('#template_list').append('<option value="' + template.id + '">' + template.name + '</option>');
-        });
-      }
-    );
+  // Update the template summary list in the modal
+  updateTemplateSummary() {
+    // Extract item names or question texts based on the item type
+    const templateItems = this.itemsType === ITEMS_TYPE.QUESTIONS
+      ? this.globalData.questions.map(question => question.question_text)
+      : this.globalData.staffing_jobs.map(job => job.name);
 
-    $('#template_list').on('change', getTemplate);
-  });
-}());
+    // Clear the old summary and add the new items
+    $('#template_summary').empty().append('<h3>Items</h3>');
+    const itemsList = $('<ul id="template_items_list"></ul>');
+    templateItems.forEach(itemValue => {
+      $(itemsList).append(`<li>${itemValue}</li>`);
+    });
+    $('#template_summary').append(itemsList);
+  }
+
+  // Load the list of available templates into the dropdown
+  loadTemplateList() {
+    $.getJSON(this.templatesBaseUrl, data => {
+      data.forEach(template => {
+        $('#template_list').append(`<option value="${template.id}">${template.name}</option>`);
+      });
+    });
+  }
+}
