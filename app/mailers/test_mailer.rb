662c98585e84069a99dd8b61b6dfca3e9899a870
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-19 08:59:38 +0100

    Set test mailer to send plaintext emails

--- app/mailers/test_mailer.rb
+++ app/mailers/test_mailer.rb
@@ -2,6 +2,6 @@ class TestMailer < ApplicationMailer
   default reply_to: 'IT <it@bedlamtheatre.co.uk>'
 
   def test_email(email_address)
-    return mail(to: email_address, subject: 'Check In')
+    return mail(to: email_address, subject: 'Check In', content_type: "text/plain")
   end
 end
