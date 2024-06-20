72ca2817f394a5dcef1cb3463cd14f2c0e3c02ae
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-20 09:32:06 +0100

    Remove unused vendor scripts that are now just taken from package.json

--- app/javascript/admin.js
+++ app/javascript/admin.js
@@ -4,6 +4,7 @@ import './src/vendor/adminlte.customised.js'
 
 import './src/admin/cookies.js'
 
+// This is a Bootstrap 4 install, which is still needed for adminLte
 import './src/vendor/bootstrap.bundle.min.js'
 
 import './src/admin/user_modal_script.js'
