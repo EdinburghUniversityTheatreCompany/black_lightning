56a263b06dfa658365ea9d8a082d8f66c283613d
Mick Zijdel <mickzijdel@hotmail.com>
2024-06-21 10:48:17 +0100

    Remove redundant * in require fields from simple_form as it is added by tabler css

--- config/initializers/simple_form_bootstrap.rb
+++ config/initializers/simple_form_bootstrap.rb
@@ -20,8 +20,8 @@ SimpleForm.setup do |config|
   # Define the default class of the input wrapper of the boolean input.
   config.boolean_label_class = 'form-check-label'
 
-  # How the label text should be generated altogether with the required text.
-  config.label_text = lambda { |label, required, explicit_label| "#{label} #{required}" }
+  # How the label text should be generated altogether with any labels. The required * is added by tabler css, so it is removed from here.
+  config.label_text = lambda { |label, required, explicit_label| label }
 
   # Define the way to render check boxes / radio buttons with labels.
   config.boolean_style = :inline
