5acc91574b6d90dc82aeb81a02d6ccbb66f3d79c
Mick Zijdel <mickzijdel@hotmail.com>
2024-11-11 15:46:22 +0000

    Only normalize sxxxxxxx@sms.ed.ac.uk style emails to @ed.ac.uk. This leaves the initial.last-name style email addresses intact

--- db/migrate/20241027165437_normalize_email_domains_to_ed_ac_uk.rb
+++ db/migrate/20241027165437_normalize_email_domains_to_ed_ac_uk.rb
@@ -1,6 +1,6 @@
 class NormalizeEmailDomainsToEdAcUk < ActiveRecord::Migration[7.1]
   def change
-    User.find_each do |user|
+    User.where(email: /^\s*s\d{7}@sms\.ed\.ac\.uk\s*$/).find_each do |user|
       user.update(email: user.email.gsub("@sms.ed.ac.uk", "@ed.ac.uk"))
     end
   end
