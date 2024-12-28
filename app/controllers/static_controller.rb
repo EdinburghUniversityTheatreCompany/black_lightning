52fa62f6b7ed4c4c26d5b6c29adab3866d9ba2c2
Mick Zijdel <mickzijdel@hotmail.com>
2024-12-28 11:35:28 +0100

    Remove contact form and complaint form

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -22,33 +22,4 @@ class StaticController < ApplicationController
 
     @standard_carousel_items = CarouselItem.where(carousel_name: 'Home').active_and_ordered.includes(image_attachment: :blob)
   end
-
-  def contact_form_send
-    # I do not know how to deliberately fail the captcha, as the entire check is disabled in testing.
-    # :nocov:
-    unless verify_recaptcha(action: 'submit_contact_form', score: 1)
-      render 'contact'
-
-      return
-    end
-    # :nocov:
-
-    # If it passed the captcha, send the email.
-  
-    sender_email = params[:contact][:email]
-    name = params[:contact][:name]
-    recipient_email = params[:contact][:recipient]
-    subject = params[:contact][:subject]
-    message = params[:contact][:message]
-
-    ContactFormMailer.contact_form_mail(sender_email, recipient_email, name, subject, message).deliver_later
-
-    success_message = "Email with subject \"#{subject}\" has been successfully sent to #{recipient_email}"
-    helpers.append_to_flash(:success, success_message)
-
-    respond_to do |format|
-      format.html { redirect_to(static_url('contact')) }
-      # format.json { render json: success_message }
-    end
-  end
 end
