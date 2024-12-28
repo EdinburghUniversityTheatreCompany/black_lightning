fccee074683126c1db93cd3b5468241d083fb128
Mick Zijdel <mickzijdel@hotmail.com>
2024-12-28 11:35:28 +0100

    Remove contact form and complaint form

--- app/controllers/complaints_controller.rb
+++ app/controllers/complaints_controller.rb
@@ -4,23 +4,6 @@ class ComplaintsController < ApplicationController
 
   load_and_authorize_resource
 
-  def create
-    # I do not know how to deliberately fail the captcha, as the entire check is disabled in testing.
-    # :nocov:
-    unless verify_recaptcha(action: 'submit_complaint', score: 1)
-      render 'new'
-
-      return
-    end
-    # :nocov:
-
-    @complaint.resolved = false
-
-    super
-
-    ComplaintsMailer.new_complaint(@complaint).deliver_later unless @complaint.new_record?
-  end
-
   private
 
   def create_redirect_url
