d114426ec2f742da2ef6d8ed0b3d77d46b2d7dad
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-12 19:11:40 +0100

    fix: Safe accessing of secrets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,15 +77,15 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address: "smtp.mailersend.net",
-    port: 587,
-    domain: "notify.bedlamtheatre.co.uk",
-    user_name: Rails.application.credentials[:mailsender][Rails.env.to_sym][:user],
-    password: Rails.application.credentials[:mailsender][Rails.env.to_sym][:password],
-    authentication: :login,
-    enable_starttls_auto: true
-  }
+config.action_mailer.smtp_settings = {
+  address: "smtp.mailersend.net",
+  port: 587,
+  domain: "notify.bedlamtheatre.co.uk",
+  user_name: Rails.application.credentials.dig(:mailsender, Rails.env.to_sym, :user),
+  password: Rails.application.credentials.dig(:mailsender, Rails.env.to_sym, :password),
+  authentication: :login,
+  enable_starttls_auto: true
+}
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
