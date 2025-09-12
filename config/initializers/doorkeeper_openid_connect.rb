a0f75e7e532c64847555046e1e65d1034d59fee5
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-12 19:11:40 +0100

    fix: Safe accessing of secrets

--- config/initializers/doorkeeper_openid_connect.rb
+++ config/initializers/doorkeeper_openid_connect.rb
@@ -1,87 +1,90 @@
 # frozen_string_literal: true
 
-raise Exception.new, "The configuration for openid_connect is not included in the credentials.yml.enc file for the '#{Rails.env.to_sym}' environment" unless Rails.application.credentials[:openid_connect].try(Rails.env.to_sym).try(:issuer).present?
+creds = Rails.application.credentials.dig(:openid_connect, Rails.env.to_sym)
+if creds.present? && creds[:issuer].present?
+  Doorkeeper::OpenidConnect.configure do
+    issuer creds[:issuer]
 
-Doorkeeper::OpenidConnect.configure do
-  issuer Rails.application.credentials[:openid_connect][Rails.env.to_sym][:issuer]
+    signing_key File.read("#{Rails.root}/config/credentials/openid_signing_key")
 
-  signing_key File.read("#{Rails.root}/config/credentials/openid_signing_key")
+    subject_types_supported [ :public ]
 
-  subject_types_supported [ :public ]
-
-  resource_owner_from_access_token do |access_token|
-    # Example implementation:
-    User.find_by(id: access_token.resource_owner_id)
-  end
-
-  auth_time_from_resource_owner do |resource_owner|
-    # Example implementation:
-    resource_owner.current_sign_in_at
-  end
-
-  reauthenticate_resource_owner do |resource_owner, return_to|
-    # Example implementation:
-    store_location_for resource_owner, return_to
-    sign_out resource_owner
-    redirect_to new_user_session_url
-  end
-
-  # Depending on your configuration, a DoubleRenderError could be raised
-  # if render/redirect_to is called at some point before this callback is executed.
-  # To avoid the DoubleRenderError, you could add these two lines at the beginning
-  #  of this callback: (Reference: https://github.com/rails/rails/issues/25106)
-  #   self.response_body = nil
-  #   @_response_body = nil
-  select_account_for_resource_owner do |resource_owner, return_to|
-    self.response_body = nil
-    @_response_body = nil
-
-    # Example implementation:
-    store_location_for resource_owner, return_to
-    redirect_to account_select_url
-  end
-
-  subject do |resource_owner, application|
-    # Example implementation:
-    resource_owner.id
-
-    # or if you need pairwise subject identifier, implement like below:
-    # Digest::SHA256.hexdigest("#{resource_owner.id}#{URI.parse(application.redirect_uri).host}#{'your_secret_salt'}")
-  end
-
-  # Protocol to use when generating URIs for the discovery endpoint,
-  # for example if you also use HTTPS in development
-  # protocol do
-  #   :https
-  # end
-
-  # Expiration time on or after which the ID Token MUST NOT be accepted for processing. (default 120 seconds).
-  # expiration 600
-
-  # Example claims:
-  claims do
-    claim :email do |resource_owner, scopes|
-      scopes.exists?(:email) ? resource_owner.email : ""
+    resource_owner_from_access_token do |access_token|
+      # Example implementation:
+      User.find_by(id: access_token.resource_owner_id)
     end
 
-    claim :email_verified do |resource_owner, scopes|
-      scopes.exists?(:email) ? true : ""
+    auth_time_from_resource_owner do |resource_owner|
+      # Example implementation:
+      resource_owner.current_sign_in_at
     end
 
-    claim :full_name do |resource_owner, scopes|
-      scopes.exists?(:profile) ? resource_owner.name_or_default : ""
+    reauthenticate_resource_owner do |resource_owner, return_to|
+      # Example implementation:
+      store_location_for resource_owner, return_to
+      sign_out resource_owner
+      redirect_to new_user_session_url
     end
 
-    claim :name do |resource_owner, scopes|
-      scopes.exists?(:profile) ? resource_owner.name_or_default : ""
+    # Depending on your configuration, a DoubleRenderError could be raised
+    # if render/redirect_to is called at some point before this callback is executed.
+    # To avoid the DoubleRenderError, you could add these two lines at the beginning
+    #  of this callback: (Reference: https://github.com/rails/rails/issues/25106)
+    #   self.response_body = nil
+    #   @_response_body = nil
+    select_account_for_resource_owner do |resource_owner, return_to|
+      self.response_body = nil
+      @_response_body = nil
+
+      # Example implementation:
+      store_location_for resource_owner, return_to
+      redirect_to account_select_url
     end
 
-    claim :family_name do |resource_owner, scopes|
-      scopes.exists?(:profile) ? resource_owner.last_name : ""
+    subject do |resource_owner, application|
+      # Example implementation:
+      resource_owner.id
+
+      # or if you need pairwise subject identifier, implement like below:
+      # Digest::SHA256.hexdigest("#{resource_owner.id}#{URI.parse(application.redirect_uri).host}#{'your_secret_salt'}")
     end
 
-    claim :given_name do |resource_owner, scopes|
-      scopes.exists?(:profile) ? resource_owner.first_name : ""
+    # Protocol to use when generating URIs for the discovery endpoint,
+    # for example if you also use HTTPS in development
+    # protocol do
+    #   :https
+    # end
+
+    # Expiration time on or after which the ID Token MUST NOT be accepted for processing. (default 120 seconds).
+    # expiration 600
+
+    # Example claims:
+    claims do
+      claim :email do |resource_owner, scopes|
+        scopes.exists?(:email) ? resource_owner.email : ""
+      end
+
+      claim :email_verified do |resource_owner, scopes|
+        scopes.exists?(:email) ? true : ""
+      end
+
+      claim :full_name do |resource_owner, scopes|
+        scopes.exists?(:profile) ? resource_owner.name_or_default : ""
+      end
+
+      claim :name do |resource_owner, scopes|
+        scopes.exists?(:profile) ? resource_owner.name_or_default : ""
+      end
+
+      claim :family_name do |resource_owner, scopes|
+        scopes.exists?(:profile) ? resource_owner.last_name : ""
+      end
+
+      claim :given_name do |resource_owner, scopes|
+        scopes.exists?(:profile) ? resource_owner.first_name : ""
+      end
     end
   end
+else
+  Rails.logger.warn("Skipping Doorkeeper OpenID Connect configuration: missing credentials for #{Rails.env}") if defined?(Rails)
 end
