b27827aeaaf74b015e9869f60213fd135bffc6e8
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-12 19:12:01 +0100

    fix: Duplicate healthcheck

--- config/routes.rb
+++ config/routes.rb
@@ -6,10 +6,6 @@ ChaosRails::Application.routes.draw do
   use_doorkeeper
   use_doorkeeper_openid_connect
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
   root to: "static#home"
 
   match "*path", to: "application#options", via: :options
