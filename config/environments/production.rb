a58b9e9e660a3d6b4da0b2e2a18db5d005aca8c0
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-13 09:35:14 +0100

    fix: Configure solid_queue properly to run with Puma

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,9 +66,9 @@ Rails.application.configure do
   # Use a different cache store in production.
   # config.cache_store = :mem_cache_store
 
-  # Use a real queuing backend for Active Job (and separate queues per environment).
-
-  # config.active_job.queue_name_prefix = "chaos_rails_production"
+  # Replace the default in-process and non-durable queuing backend for Active Job.
+  config.active_job.queue_adapter = :solid_queue
+  config.solid_queue.connects_to = { database: { writing: :queue } }
 
   config.action_mailer.perform_caching = false
 
