03c4007f504a136818f5b5cc141a56dfc1b3c7fd
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-13 11:21:28 +0100

    feat: Setup daily job (previously in crontab) to run every day with a container

---
+++ bash_scripts/daily_job.sh
@@ -0,0 +1,17 @@
+# Notify the marketing manager if there are any marketing creatives sign ups
+bundle exec rails marketing_creatives:notify_of_new_sign_ups
+
+# Clean up personal info in accordance with the privacy policy.
+bundle exec rails users:clean_up_personal_info
+
+# Purge unattached blobs
+bundle exec rails active_storage:purge_unattached
+
+# Expire old debts
+bundle exec rake debt:expire_outdated_debt
+
+# Notify debtors
+bundle exec rake debt:notify_debtors 
+
+# Let Honeybadger know we did it.
+curl https://api.honeybadger.io/v1/check_in/wqI0PL
