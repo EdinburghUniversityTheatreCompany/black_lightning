3eeb29948c7972bc17ee796e6b2da6f3fd8caf65
Mick Zijdel <mickzijdel@hotmail.com>
2025-09-27 17:40:39 +0100

    fix: Logout button now in navbar and it works

--- app/javascript/src/admin/user_modal_script.js
+++ app/javascript/src/admin/user_modal_script.js
@@ -1,8 +1,7 @@
 
-//Script to display a modal that asks the user if they want to view their profile or sign out when they click on their name or profile picture in the menu.
-function userModal(title, message, user_profile_url, sign_out_url) {
-  // Treat confirmation as wanting to view the user profile
-  // and denial as wanting to sign out.
+//Script to display a modal that asks the user if they want to view their profile when they click on their name or profile picture in the menu.
+function userModal(title, message, user_profile_url) {
+  // Treat confirmation as wanting to view the user profile.
   Swal.fire({
     // Define the modal's properties
     icon: 'question',
@@ -12,32 +11,11 @@ function userModal(title, message, user_profile_url, sign_out_url) {
     confirmButtonText: 'View User Profile',
     showCloseButton: true,
     showCancelButton: true,
-    cancelButtonText: "Close",
-    showDenyButton: true,
-    denyButtonText: 'Log Out'
+    cancelButtonText: "Close"
   // Determine how to respond to user input.
   }).then((result) => {
     if (result.isConfirmed) {
       location.assign(user_profile_url)
-    } else if (result.isDenied) {
-      // Sign the user out.
-      $.ajax({
-        url: sign_out_url,
-        type: "DELETE",
-        headers: {
-          'X-CSRF-Token': $('meta[name=csrf-token]').attr('content')
-        }
-      }).done(function () {
-        // And show a confirmation message.
-        Swal.fire({
-          icon: 'success',
-          title: 'Logged out',
-          text: 'You were successfully logged out. See you soon!'
-        }).then(() => {
-          location.assign('/')
-        })
-      });
-
     }
   })
 }
@@ -51,7 +29,7 @@ window.addEventListener('load', function() {
     element.addEventListener('click', function() {
       userModal(element.dataset.nameAttr,
         "You're currently logged in. Click view user profile to change your name, password, see shows that you're involved in, and more.",
-        element.dataset.currentUserPath, element.dataset.destroyUserPath)
+        element.dataset.currentUserPath)
     });
   }
 });
