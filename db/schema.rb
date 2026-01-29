0f4a6640d6d2ec7628b55d66ec1fe905ce46086e
Mick Zijdel <mickzijdel@hotmail.com>
2026-01-29 11:10:14 +0000

    feat: Track student ids on users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_01_17_092735) do
+ActiveRecord::Schema[8.1].define(version: 2026_01_29_105145) do
   create_table "active_storage_attachments", charset: "utf8mb3", force: :cascade do |t|
     t.bigint "blob_id", null: false
     t.datetime "created_at", precision: nil, null: false
@@ -740,10 +740,12 @@ ActiveRecord::Schema[8.1].define(version: 2026_01_17_092735) do
     t.datetime "reset_password_sent_at", precision: nil
     t.string "reset_password_token"
     t.integer "sign_in_count", default: 0
+    t.string "student_id"
     t.datetime "updated_at", precision: nil, null: false
     t.string "username"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    t.index ["student_id"], name: "index_users_on_student_id"
   end
 
   create_table "users_roles", id: false, charset: "utf8mb3", collation: "utf8mb3_unicode_ci", force: :cascade do |t|
