a3891fe9f78cfa0857ed9fd17a88d99325453fd5
Mick Zijdel <mickzijdel@hotmail.com>
2026-02-04 09:44:01 +0000

    feat: Update welcome emails to have a template tag with the link to the profilec ompletion url

--- app/mailers/users_mailer.rb
+++ app/mailers/users_mailer.rb
@@ -6,6 +6,13 @@ class UsersMailer < ApplicationMailer
     @user = user
     @subject = "Welcome to Bedlam Theatre"
 
+    # Generate profile completion URL for incomplete profiles
+    @profile_completion_url = if @user.profile_incomplete?
+      profile_completion_url(token: @user.profile_completion_token, protocol: "https")
+    else
+      new_user_session_url(protocol: "https")
+    end
+
     # Select editable block based on membership status
     block_name = @user.has_role?(:member) ? "Email - Welcome Email (Member)" : "Email - Welcome Email (Non-Member)"
     @editable_block = Admin::EditableBlock.find_by_name!(block_name)
