af3ab04fb59d03f580b990d782c0840917750b52
Mick Zijdel <mickzijdel@hotmail.com>
2026-02-06 11:28:33 +0000

    fix: Event factory venue linking issue

--- test/factories/event_factory.rb
+++ test/factories/event_factory.rb
@@ -44,8 +44,8 @@ FactoryBot.define do
     pretix_shown { [ true, false ].sample }
     pretix_view  { [ "list", "week", "month" ].sample }
 
-    # Use first venue from DB, create one if none exist
-    venue { Venue.first || FactoryBot.create(:venue) }
+    # Use fixture venue - fixtures are loaded in tests
+    venue_id { Venue.first&.id }
 
     transient do
       team_member_count { 0 }
