58b41d471b2f159eba9251458c0adf2a0b506cf1
Mick Zijdel <mickzijdel@hotmail.com>
2026-02-07 11:52:00 +0000

    fix: Add end date to a created show to prevent test error

--- test/helpers/workshop_counter_test.rb
+++ test/helpers/workshop_counter_test.rb
@@ -2,8 +2,8 @@ require "test_helper"
 
 class WorkshopCounterTest < ActionView::TestCase
   test "get workshop count" do
-    FactoryBot.create_list(:workshop, 2, is_public: true, end_date: Date.current.advance(days: 1))
-    FactoryBot.create :workshop, end_date: Date.current.advance(days: -1)
+    FactoryBot.create_list(:workshop, 2, is_public: true, start_date: Date.current, end_date: Date.current.advance(days: 1))
+    FactoryBot.create :workshop, start_date: Date.current.advance(days: -2), end_date: Date.current.advance(days: -1)
     FactoryBot.create :workshop, is_public: false
     assert_equal 2, WorkshopCounter.workshop_count
   end
