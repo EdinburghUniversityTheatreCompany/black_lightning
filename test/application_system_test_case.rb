8815c25317eb5ebbec867415d968580f6c361bc4
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-01 14:47:54 +0100

    fix: Selenium config so it works on CI

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,9 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :chrome, screen_size: [ 1400, 1400 ]
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+    args: %w[--no-sandbox --disable-dev-shm-usage --disable-gpu]
+  }
   include Warden::Test::Helpers
 
   # Select a value in a Tom Select widget by visible text.
