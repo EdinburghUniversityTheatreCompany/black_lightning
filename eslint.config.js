93ec8e93d07d309f048fea7323e3db6991566aa1
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-01 16:50:59 +0100

    feat: Setup eslint

---
+++ eslint.config.js
@@ -0,0 +1,26 @@
+import js from "@eslint/js";
+import globals from "globals";
+
+export default [
+  js.configs.recommended,
+  {
+    files: [
+      "app/javascript/controllers/**/*.js",
+      "app/javascript/lib/**/*.js",
+    ],
+    languageOptions: {
+      ecmaVersion: 2022,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        Swal: "readonly",   // SweetAlert2 loaded as global
+      },
+    },
+    rules: {
+      "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+      "no-console": "warn",
+      "prefer-const": "error",
+      "no-var": "error",
+    },
+  },
+];
