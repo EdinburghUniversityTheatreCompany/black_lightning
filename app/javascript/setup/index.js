1a8f521f118e0ffb3ea5d61849228ed6b5427cab
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-01 20:43:57 +0100

    fix: Add FontAwesome to scss files to prevent Flash of Unstyled Content

--- app/javascript/setup/index.js
+++ app/javascript/setup/index.js
@@ -16,6 +16,3 @@ Turbo.session.drive = false;
 import * as ActiveStorage from "@rails/activestorage"
 ActiveStorage.start()
 
-import "@fortawesome/fontawesome-free/css/fontawesome.css"
-import "@fortawesome/fontawesome-free/css/solid.css"
-import "@fortawesome/fontawesome-free/css/brands.css"
