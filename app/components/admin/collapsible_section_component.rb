b43c228d9be81e209fb0d9329f681a8f34dfe223
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-02 00:12:35 +0100

    fix: Use CollapsibleSectionComponent for card widgets as they're basically identical

--- app/components/admin/collapsible_section_component.rb
+++ app/components/admin/collapsible_section_component.rb
@@ -1,10 +1,11 @@
 class Admin::CollapsibleSectionComponent < ViewComponent::Base
-  def initialize(title:, card_class: "", body_class: "", header_class: "", title_right: nil, start_open: false)
+  def initialize(title:, card_class: "", body_class: "", header_class: "", title_right: nil, start_open: false, flush: false)
     @title = title
     @card_class = card_class
     @body_class = body_class
     @header_class = header_class
     @title_right = title_right
     @start_open = start_open
+    @flush = flush
   end
 end
