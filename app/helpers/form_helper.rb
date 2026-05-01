54068ad1706c766bfb56bba96c6dffc2d2eee4f2
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-01 22:07:42 +0100

    feat: Detect controller namespace in form_helper to decide which form options to use

--- app/helpers/form_helper.rb
+++ app/helpers/form_helper.rb
@@ -1,18 +1,34 @@
 module FormHelper
   def horizontal_form_options
-    {
-      wrapper: :tailwind_horizontal_form,
-      wrapper_mappings: {
-        boolean:       :tailwind_horizontal_boolean,
-        check_boxes:   :tailwind_horizontal_collection,
-        date:          :tailwind_horizontal_multi_select,
-        datetime:      :tailwind_horizontal_multi_select,
-        file:          :tailwind_horizontal_file,
-        radio_buttons: :tailwind_horizontal_collection,
-        range:         :tailwind_horizontal_range,
-        time:          :tailwind_horizontal_multi_select
+    if params[:controller].to_s.start_with?("admin/")
+      {
+        wrapper: :tailwind_horizontal_form,
+        wrapper_mappings: {
+          boolean:       :tailwind_horizontal_boolean,
+          check_boxes:   :tailwind_horizontal_collection,
+          date:          :tailwind_horizontal_multi_select,
+          datetime:      :tailwind_horizontal_multi_select,
+          file:          :tailwind_horizontal_file,
+          radio_buttons: :tailwind_horizontal_collection,
+          range:         :tailwind_horizontal_range,
+          time:          :tailwind_horizontal_multi_select
+        }
       }
-    }
+    else
+      {
+        wrapper: :horizontal_form,
+        wrapper_mappings: {
+          boolean:       :horizontal_boolean,
+          check_boxes:   :horizontal_collection,
+          date:          :horizontal_multi_select,
+          datetime:      :horizontal_multi_select,
+          file:          :horizontal_file,
+          radio_buttons: :horizontal_collection,
+          range:         :horizontal_range,
+          time:          :horizontal_multi_select
+        }
+      }
+    end
   end
 
   def simple_horizontal_form_for(object, *args, &block)
