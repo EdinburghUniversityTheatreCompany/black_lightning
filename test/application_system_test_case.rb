424880922edd9a15520e662a0da20c886cee1bf1
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-01 15:01:33 +0100

    fix: Selenium config

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,9 +1,11 @@
 require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
-  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
-    args: %w[--no-sandbox --disable-dev-shm-usage --disable-gpu]
-  }
+  driven_by :selenium, using: :headless_chrome, screen_size: [ 1400, 1400 ] do |driver_options|
+    driver_options.add_argument("--no-sandbox")
+    driver_options.add_argument("--disable-dev-shm-usage")
+    driver_options.add_argument("--disable-gpu")
+  end
   include Warden::Test::Helpers
 
   # Select a value in a Tom Select widget by visible text.
