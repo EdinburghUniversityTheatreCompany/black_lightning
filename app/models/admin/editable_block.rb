ce659c7e99779e04199ef2dd5c32d31a1fbaf43c
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-03 11:33:36 +0100

    fix: Render editable block text with the appropriate wrapping classes to style it, and bust all editable_block cached_rendered_content by upping the version

--- app/models/admin/editable_block.rb
+++ app/models/admin/editable_block.rb
@@ -22,6 +22,8 @@
 #++
 
 class Admin::EditableBlock < ApplicationRecord
+  include MdHelper
+
   resourcify
   has_paper_trail meta: { version_note: :version_note }
 
@@ -45,8 +47,8 @@ class Admin::EditableBlock < ApplicationRecord
   end
 
   def cached_rendered_content
-    Rails.cache.fetch("editable_block_#{id}_#{updated_at.to_i}", expires_in: 7.days) do
-      ::Commonmarker.to_html(content || "", options: MdHelper::MARKDOWN_OPTIONS, plugins: { syntax_highlighter: nil })
+    Rails.cache.fetch("editable_block_#{id}_#{updated_at.to_i}_v2", expires_in: 7.days) do
+      render_markdown(content)
     end
   end
 
