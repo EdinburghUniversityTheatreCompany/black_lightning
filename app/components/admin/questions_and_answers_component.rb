52121e10809e0ecad33af447312ceaf8f2e80fb4
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-08 11:37:33 +0100

    fix: Make answer boxes flush agains the card edges

--- app/components/admin/questions_and_answers_component.rb
+++ app/components/admin/questions_and_answers_component.rb
@@ -1,5 +1,6 @@
 class Admin::QuestionsAndAnswersComponent < ViewComponent::Base
-  def initialize(answers:)
+  def initialize(answers:, flush: false)
     @answers = answers.includes(:question)
+    @flush = flush
   end
 end
