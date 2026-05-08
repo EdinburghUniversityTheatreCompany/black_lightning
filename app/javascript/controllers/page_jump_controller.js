62a2fd46a5c28b76ad098043da0e6d1f7f332524
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-08 16:29:30 +0100

    feat: Use current page pagination button as the input

--- app/javascript/controllers/page_jump_controller.js
+++ app/javascript/controllers/page_jump_controller.js
@@ -1,10 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = ["input"]
-
   jump() {
-    const page = parseInt(this.inputTarget.value, 10)
+    const page = parseInt(this.element.value, 10)
     if (!page || page < 1) return
 
     const url = new URL(window.location.href)
