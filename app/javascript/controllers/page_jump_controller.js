eef9955cac554252c100dfb151e1adf0ac1dc9fa
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-08 15:19:08 +0100

    feat: Add page jump pagination input

---
+++ app/javascript/controllers/page_jump_controller.js
@@ -0,0 +1,14 @@
+import { Controller } from "@hotwired/stimulus"
+
+export default class extends Controller {
+  static targets = ["input"]
+
+  jump() {
+    const page = parseInt(this.inputTarget.value, 10)
+    if (!page || page < 1) return
+
+    const url = new URL(window.location.href)
+    url.searchParams.set("page", page)
+    window.location.href = url.toString()
+  }
+}
