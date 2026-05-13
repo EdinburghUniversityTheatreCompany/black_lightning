a65c109a516289239515057759dad228febe4992
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-13 17:04:00 +0100

    fix: Dedupe newsletter subscribers before running unique migration

--- db/migrate/20260513152826_add_unique_index_to_newsletter_subscribers_email.rb
+++ db/migrate/20260513152826_add_unique_index_to_newsletter_subscribers_email.rb
@@ -1,5 +1,12 @@
 class AddUniqueIndexToNewsletterSubscribersEmail < ActiveRecord::Migration[8.1]
   def change
+    # Remove duplicate emails, keeping the most recently created record
+    execute <<~SQL
+      DELETE ns1 FROM newsletter_subscribers ns1
+      INNER JOIN newsletter_subscribers ns2
+      WHERE ns1.id < ns2.id AND ns1.email = ns2.email
+    SQL
+
     add_index :newsletter_subscribers, :email, unique: true, if_not_exists: true
   end
 end
