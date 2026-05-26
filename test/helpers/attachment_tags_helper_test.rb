c23602428f9da4d3af29e580aa429bc6c2aca543
Mick Zijdel <mickzijdel@hotmail.com>
2026-05-26 23:14:31 +0100

    chore: Replace Bootstrap shims  with references to ButtonComponent classes

--- test/helpers/attachment_tags_helper_test.rb
+++ test/helpers/attachment_tags_helper_test.rb
@@ -4,6 +4,9 @@ class AttachmentTagsHelperTest < ActionView::TestCase
   test "get_link_to_attachments_for_tag" do
     attachment_tag = attachment_tags(:rigplan)
 
-    assert_equal "<a class=\"btn btn-secondary\" href=\"/admin/attachments?q%5Battachment_tags_id_eq%5D=#{attachment_tag.id}\">View Attachments</a>", get_link_to_attachments_for_tag(attachment_tag)
+    result = get_link_to_attachments_for_tag(attachment_tag)
+    assert_includes result, "View Attachments"
+    assert_includes result, "/admin/attachments?q%5Battachment_tags_id_eq%5D=#{attachment_tag.id}"
+    assert_includes result, ButtonComponent.classes_for(variant: :secondary).split.first
   end
 end
